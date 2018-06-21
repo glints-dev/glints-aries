5300dea7108fe43d0d2c53e966c8baa862edb7e3
Fredy Yanto <fredyyanto@Fredys-MacBook-Pro.local>
2018-06-21 13:55:52 +0700

    Add Style Config

---
+++ src/Utils/StyleConfig.js
@@ -0,0 +1,13 @@
+export const Variant = {
+    DEFAULT: 'default',
+    PRIMARY: 'primary',
+    SECONDARY: 'secondary',
+    GHOST: 'ghost',
+    LINK: 'link',
+};
+
+export const Color = {
+    RED: 'red',
+    YELLOW: 'yellow',
+    BLUE: 'blue',
+};
