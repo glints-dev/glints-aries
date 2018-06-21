19bef94b5f3f3fd73cf039f8f744fd15c6ce404d
Tirta Nugraha <tirtanugraha@Tirtas-MacBook-Pro-2.local>
2018-06-21 10:01:06 +0700

    update story component

--- stories/index.js
+++ stories/index.js
@@ -5,9 +5,11 @@ import Provider from './Provider';
 import {storiesOf} from '@storybook/react';
 import IntroStory from './IntroStory';
 import ButtonStory from './ButtonStory';
-// import CardStory from './CardStory';
+import CardStory from './CardStory';
 
 storiesOf('Glints Components', module)
   .addDecorator(story => <Provider>{React.createElement(story)}</Provider>)
   .add('Introduction', () => <IntroStory />)
-  .add('Button', () => <ButtonStory />);
+  .add('Button', () => <ButtonStory />)
+  .add('Card', () => <CardStory />);
+  
