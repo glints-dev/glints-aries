7fe3a4cf7490856079e840e0913b4ab56fd6c0f2
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-21 12:53:04 +0700

    refactor(Button): props controlled button style controlling button appearance by passing a props

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,35 +1,85 @@
 /* @flow */
 
 import React, { Fragment } from 'react';
+import styled, {css} from 'styled-components';
 import CtaButton from './CtaButton';
 import JobCardButton from './JobCardButton';
 import SecondaryButton from './SecondaryButton';
 
+type ButtonVariant = 'default' | 'cta';
+type ButtonType = 'primary' | 'secondary' | 'info'
+
 type Props = {
-	variant?: string
+	disabled: boolean,
+	variant?: ButtonVariant,
+	type?: ButtonType,
 }
 
+type HandlerProps = {
+	disabled: boolean,
+	variant?: ButtonVariant,
+	type?: ButtonType
+};
+
+const buttonStyleHandler = (props: HandlerProps) => {
+  const styles: Object = {};
+
+  styles.width = props.block || props.position ? '100%' : 'auto';
+  styles.borderRadius = props.round ? '8rem' : '0rem';
+
+  switch (props.variant) {
+    case 'cta':
+      styles.border = 'solid 0.1rem rgba(0, 0, 0, 0.24)';
+      styles.borderBottomColor = 'rgba(0, 0, 0, 0.32)';
+      styles.backgroundColor = '#3d70b2';
+      styles.mouseEventBackgroundColor = '#30588c';
+      break;
+    default:
+      styles.border = 'solid 0.1rem rgba(0, 0, 0, 0.24)';
+      styles.borderBottomColor = 'rgba(0, 0, 0, 0.32)';
+      styles.backgroundColor = '#fff';
+      styles.mouseEventBackgroundColor = '#d2d2d2';
+	}
+	
+	switch (props.type) {
+		case 'primary':
+			styles.backgroundColor = 'red';
+			break;
+		default:
+			styles.backgroundColor = '#333';
+	}
+
+  styles.cursor = props.disabled ? 'not-allowed' : 'pointer';
+  styles.opacity = props.disabled ? '0.72' : '1';
+  styles.mouseEventBackgroundColor = props.disabled
+    ? styles.backgroundColor
+		: styles.mouseEventBackgroundColor;
+		
+	console.log(styles);
+
+  return styles;
+};
+
+const ButtonWrapper = styled.button`
+	appearance: none;
+	background-color: ${props => buttonStyleHandler(props).backgroundColor};
+`
+
 const Button = (props: Props) => {
-	const { variant } = props;
+	const {
+		variant,
+		type,
+		...defaultButtonProps
+	} = props;
 
 	return (
-		<Fragment>
-			{variant === 'cta' &&
-				<CtaButton onClick={props.onClick}>{props.children}</CtaButton>
-			}
-
-			{variant === 'job' && 
-				<JobCardButton onClick={props.onClick}>{props.children}</JobCardButton>
-			}
-
-			{variant === "secondary" && 
-				<SecondaryButton onClick={props.onClick}>{props.children}</SecondaryButton>
-			}
-
-			{variant === "nostyle" && 
-				<button>no style button</button>
-			}
-		</Fragment>
+		<ButtonWrapper
+			variant={variant}
+			type={type}
+			{...defaultButtonProps}
+		>
+			{props.children}
+		</ButtonWrapper>
 	);
 }
 
