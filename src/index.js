04b783214c759d10ff0ba9d1b2caa9d993f39946
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-22 11:01:30 +0700

    feat(Label): Glints label component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // @flow
 
 // @create-index
+// keep in alphabetical order
 export {default as Button} from './Button';
+export {default as Label} from './Label';
 export {Uikit as default, UikitConsumer, UikitProvider} from './Provider';
