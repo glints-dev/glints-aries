b306110d6fe60da94525fec1bf063d039e68693b
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-22 14:55:53 +0700

    exporting components

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@
 // @create-index
 // keep in alphabetical order
 export {default as Button} from './Button';
+export {default as Card} from './Card';
+export {default as Form} from './Form';
+export {default as Item} from './Item';
 export {default as Label} from './Label';
 export {Uikit as default, UikitConsumer, UikitProvider} from './Provider';
