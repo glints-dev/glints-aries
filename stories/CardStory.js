98c659eabc60f7c4e3d9f8ac4945dfd4c6b00dcd
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-25 16:04:32 +0700

    refactor(component): reuse Item component on Card

--- stories/CardStory.js
+++ stories/CardStory.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from './../src/Card';
 import JobBanner from './../src/JobBanner';
+import Item from './../src/Item';
 
 const CardStories = () => {
   return (
     <div className="doc-mainbar">
       <h1>Glints Card</h1>
-      <p>Usage: <code>{`import { Card, JobBanner } from '@glints-dev/glints-aries'`}</code></p>
+      <p>Usage: <code>{`import { Card, JobBanner, Item } from '@glints-dev/glints-aries'`}</code></p>
       <p style={{color: 'red', fontSize: 20}}>Beta</p>
       <table className="doc-table">
         <thead>
@@ -20,10 +21,7 @@ const CardStories = () => {
         <tbody>
           <tr>
             <td style={{padding: 20}}>
-              <Card 
-                CompanyLogo={'https://upload.wikimedia.org/wikipedia/commons/d/df/GLINTS_LOGO293.png'}
-                CompanyName={'Glints'}
-                JobTitle={'Software Engineer'}
+              <Card
                 JobCategory={'Design'}
                 JobSalary={'SGD 1000 - 2000'}
                 JobLocation={'Singapore'}
@@ -33,12 +31,18 @@ const CardStories = () => {
                   imgUrl={"http://www.pastamania.com.sg/wp-content/uploads/2016/02/new_content_image_careers-2.jpg"}
                   variant="primary"
                 >Apply</JobBanner>
+
+                <Item
+                  title="Item title"
+                  subtitle="Item subtitle"
+                  url="https://glints.id"
+                  imgUrl="https://upload.wikimedia.org/wikipedia/commons/d/df/GLINTS_LOGO293.png"
+                  isExternal={true}
+                  paddingSize={'1.4em'}
+                />
               </Card>
             </td>
-            <td style={{verticalAlign: 'top', paddingTop: 20, width: '100%'}}><pre>{`<Card 
-  CompanyLogo={'string url'}
-  CompanyName={'Glints'}
-  JobTitle={'Software Engineer'}
+            <td style={{verticalAlign: 'top', paddingTop: 20, width: '100%'}}><pre>{`<Card
   JobCategory={'Design'}
   JobSalary={'SGD 1000 - 2000'}
   JobLocation={'Singapore'}
@@ -50,6 +54,15 @@ const CardStories = () => {
   >
     Apply
   </JobBanner>
+
+  <Item
+    title="Item title"
+    subtitle="Item subtitle"
+    url="https://glints.id"
+    imgUrl="string"
+    isExternal={true}
+    paddingSize={'1.4em'}
+  />
 </Card>`}</pre></td>
           </tr>
         </tbody>
