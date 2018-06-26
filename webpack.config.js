83598e05efeac4528ad4ef36f4a39f437a0fd868
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-26 21:18:46 +0700

    fix(webpack): fix file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,18 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name]-[hash].[ext]',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(png|jpg|gif|svg|woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
