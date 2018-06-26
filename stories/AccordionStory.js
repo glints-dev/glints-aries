524032f6eed3d18f1b49867aee27d3004f363cd2
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-26 21:52:38 +0700

    fix story label

--- stories/AccordionStory.js
+++ stories/AccordionStory.js
@@ -20,7 +20,7 @@ class AccordionStory extends Component {
   render() {
     return (
       <div className="doc-mainbar">
-        <h1>Glints Label</h1>
+        <h1>Glints Accordion</h1>
         <p>usage: <pre>{`import Accordion, { AccordionItem, AccordionHeader, Label } '@glints-dev/glints-aries'`}</pre></p>
   
         <table className="doc-table">
