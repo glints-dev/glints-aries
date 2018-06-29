b9f1763e8a5f8dc15b4ca2784a19ff1f0d062a48
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-29 10:12:49 +0700

    feat(component): FlexCenter component utilities

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export {default as Banner} from './Banner';
 export {default as Button} from './Button';
 export {default as Card} from './Card';
 export {default as Divider} from './Divider';
+export {default as FlexCenter} from './FlexCenter';
 export {default as Heading} from './Heading';
 export {default as Icon, Icons} from './Icon';
 export {default as Item} from './Item';
