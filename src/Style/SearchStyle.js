7f1ec065e7aa2c00f4ef4fe1128bf923a9d60b3f
Fredy Yanto <fredyyanto@Fredys-MacBook-Pro.local>
2018-07-02 17:28:58 +0700

    Added Box Shadow on Search Component

--- src/Style/SearchStyle.js
+++ src/Style/SearchStyle.js
@@ -6,8 +6,9 @@ export const SearchContainer = styled.div`
     align-items: center;
     width: 100%;
     border: 2px solid ${SecondaryColor.lightgrey};
-    padding: .6em .8em;
+    padding: .4em .8em;
     background: ${SecondaryColor.white};
+    box-shadow: 0 10px 15px 0 rgba(0,0,0,0.05);
 `;
 
 export const SearchItemWrapper = styled.div`
