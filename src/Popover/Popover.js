a8db7d61098ceb64ed82c12d2afd7de50a76ac14
Jason Loh <ksloh1991@gmail.com>
2018-07-26 17:52:46 +0800

    - add new props to change margin for the button

--- src/Popover/Popover.js
+++ src/Popover/Popover.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { ChildHolder, ButtonContainer, PopOverContent } from '../Style/PopoverStyle';
 
 class Popover extends Component <Props, State> {
+  static defaultProps = {
+    margin: '',
+  }
+
   constructor() {
     super();
     this.state = {
@@ -22,16 +26,19 @@ class Popover extends Component <Props, State> {
     this.setState({ isOpen: false });
   }
 
-  renderChildren = children => (
-    <ChildHolder onClick={this.handleOnClick}>
-      {children}
-    </ChildHolder>
-  )
+  renderChildren = (children) => {
+    const { margin } = this.props;
+    return (
+      <ChildHolder onClick={this.handleOnClick} margin={margin}>
+        {children}
+      </ChildHolder>
+    );
+  }
 
   render() {
     const { isOpen } = this.state;
     const { content, children } = this.props;
-    
+
     return (
       <ButtonContainer
         tabIndex="0"
