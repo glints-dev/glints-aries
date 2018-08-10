9723006319bcc43177306346d8896cb2df273ce2
Fredy Yanto <fredyyanto@Fredys-MacBook-Pro.local>
2018-08-10 16:00:15 +0700

    Fix container size

--- src/Style/ContainerStyle.js
+++ src/Style/ContainerStyle.js
@@ -10,42 +10,34 @@ export const Outter = styled.div`
 export const Inner = styled.div`
     @media (min-width: ${size.desktopL}) { 
         width: 1400px;
-        background: red;
     }
 
     @media (min-width: ${size.desktopM}) and (max-width: 1439px) { 
         width: 1200px;
-        background: green;
     }
 
     @media (min-width: ${size.desktopS}) and (max-width: 1259px) { 
         width: 960px;
-        background: yellow;
     }
 
     @media (min-width: ${size.tablet}) and (max-width: 1023px) { 
         width: 720px;
-        background: grey;
     }
 
     @media (min-width: ${size.mobileL}) and (max-width: 767px) { 
         width: 600px;
-        background: orange;
     }
 
     @media (min-width: ${size.mobileM}) and (max-width: 639px) { 
         width: 440px;
-        background: purple;
     }
 
     @media (min-width: ${size.mobileS}) and (max-width: 479px) { 
         width: 300px;
-        background: pink;
     }
 
     @media ${Device.mobileS} { 
         width: 300px;
         padding: 0 1rem;
-        background: pink;
     }
 `;
