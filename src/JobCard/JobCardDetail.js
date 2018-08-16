dbfca3cf31b07f67b17ddfdab6dd042988f30b18
Fredy Yanto <fredyyanto@Fredys-MacBook-Pro.local>
2018-08-16 11:33:34 +0700

    Fix Job Card Bullet

--- src/JobCard/JobCardDetail.js
+++ src/JobCard/JobCardDetail.js
@@ -6,22 +6,12 @@ import { JobDetail } from '../Style/JobCardStyle';
 const JobCardDetail = ({ details }: Props) => (
   <JobDetail>
     <ul>
-      <li>
-        <Icon name="dot" color="#c6c6c6" size="6" />
-        { details[0] }
-      </li>
-      <li>
-        <Icon name="dot" color="#c6c6c6" size="6" />
-        { details[1] }
-      </li>
-      <li>
-        <Icon name="dot" color="#c6c6c6" size="6" />
-        { details[2] }
-      </li>
-      <li>
-        <Icon name="dot" color="#c6c6c6" size="6" />
-        { details[3] }
-      </li>
+      {details.map(data => (
+        <li key={data}>
+          <Icon name="dot" color="#c6c6c6" size="6" />
+          { data }
+        </li>
+      ))}
     </ul>
   </JobDetail>
 );
