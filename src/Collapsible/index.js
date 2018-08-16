780943aa5f292ee1ce4c272b1ddcfd8959d3bbbf
Tirta Nugraha <dev.nugrata@gmail.com>
2018-08-16 09:53:25 +0700

    feat(component): adding collapsible component

---
+++ src/Collapsible/index.js
@@ -0,0 +1,8 @@
+// @flow
+
+import Collapsible, { CollapsibleHead, CollapsibleBody } from './Collapsible';
+
+Collapsible.Head = CollapsibleHead;
+Collapsible.Body = CollapsibleBody;
+
+export default Collapsible;
