39d3bf483eb2d26931fdc4ec698730f55dd5136f
Tirta Nugraha <dev.nugrata@gmail.com>
2018-08-28 14:03:11 +0700

    refactor(component): Button component refactor. adding id attribute support

--- src/Button/PrimaryButton.js
+++ src/Button/PrimaryButton.js
@@ -4,22 +4,29 @@ import React from 'react';
 import { PrimaryContainer, PrimaryBtn } from '../Style/ButtonStyle';
 
 const PrimaryButton = ({
+  id,
   children,
   theme,
   className,
   ...defaultButtonProps
 }: Props) => (
   <PrimaryContainer theme={theme} className={className}>
-    <PrimaryBtn className="primaryButton" theme={theme} {...defaultButtonProps}>
+    <PrimaryBtn className={`primaryButton ${className}`} id={id} theme={theme} {...defaultButtonProps}>
       {children}
     </PrimaryBtn>
   </PrimaryContainer>
 );
 
 type Props = {
+  id: string,
   theme: string,
   children: React$Node,
   className: string,
 }
+
+PrimaryButton.defaultProps = {
+  id: '',
+  className: '',
+};
 
 export default PrimaryButton;
