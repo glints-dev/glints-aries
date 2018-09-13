9bbdf5915e37dd351238181aaf2c666af87ceb34
Jason Loh <ksloh1991@gmail.com>
2018-09-13 13:58:47 +0800

    - remove unused code

--- stories/MobileMenuStory.js
+++ stories/MobileMenuStory.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import MobileMenu from '../src/MobileMenu';
 
-import { Icon } from '../src/Icon';
 import ProfilePictureShadow from '../src/ProfilePictureShadow';
 import Label from '../src/Label';
 
