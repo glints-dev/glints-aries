1ccf0b60cc490e338040e152baa1d637af58084d
Jason Loh <ksloh1991@gmail.com>
2018-09-27 16:44:12 +0800

    - change glints-container media query

--- src/Style/GlintsContainerStyle.js
+++ src/Style/GlintsContainerStyle.js
@@ -4,37 +4,40 @@ import { size, Device } from '../Utils/StyleConfig';
 const GlintsContainer = styled.div`
   position: relative;
   margin: 0 auto;
+  width: 100vw;
 
   @media (min-width: ${size.desktopL}) { 
     width: 1400px;
+    padding: 0 20px;
   }
 
   @media (min-width: ${size.desktopM}) and (max-width: 1439px) { 
     width: 1200px;
+    padding: 0 30px;
   }
 
   @media (min-width: ${size.desktopS}) and (max-width: 1259px) { 
-    width: 960px;
+    padding: 0 32px;
   }
 
   @media (min-width: ${size.tablet}) and (max-width: 1023px) { 
-    width: 720px;
+    padding: 0 32px;
   }
 
   @media (min-width: ${size.mobileL}) and (max-width: 767px) { 
-    width: 600px;
+    padding: 0 24px;
   }
 
   @media (min-width: ${size.mobileM}) and (max-width: 639px) { 
-    width: 440px;
+    padding: 0 20px;
   }
 
   @media (min-width: ${size.mobileS}) and (max-width: 479px) { 
-    width: 300px;
+    padding: 0 10px;
   }
 
   @media ${Device.mobileS} { 
-    width: 300px;
+    padding: 0 10px;
   }
   
   *,
