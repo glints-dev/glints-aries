8ef78c16ccb55d0a32ae5c9b0db315d558d5d707
fredyyanto <fredy4th@gmail.com>
2018-10-12 15:25:17 +0700

    New Accordion

--- src/Display/Accordion/Accordion.js
+++ src/Display/Accordion/Accordion.js
@@ -1,26 +1,91 @@
 /* @flow */
 
-import React from 'react';
-import {
-  AccordionContainer,
-} from '../../Style/Display/AccordionStyle';
+import React, { Component } from 'react';
 
-const Accordion = (props: Props) => {
-  const {
-    children,
-    className,
-  } = props;
+import AccordionPanel from './AccordionPanel';
 
-  return (
-    <AccordionContainer className={className}>
-      {children}
-    </AccordionContainer>
-  );
-};
+import { AccordionContainer } from '../../Style/Display/AccordionStyle';
+
+class Accordion extends Component <Props, State> {
+  state = {
+    currIndex: -1,
+    isOpenSingleItem: false,
+  }
+
+  handleOpen = (index) => {
+    const { currIndex } = this.state;
+
+    this.setState({
+      currIndex: currIndex === index ? -1 : index,
+    });
+  }
+
+  handleOpenSingleItem = () => {
+    const { isOpenSingleItem } = this.state;
+
+    this.setState({
+      isOpenSingleItem: !isOpenSingleItem,
+    });
+  }
+
+  renderMultipleItem() {
+    const { children } = this.props;
+    const { currIndex } = this.state;
+
+    return (
+      children.map((data, index) => (
+        <AccordionPanel
+          key={index}
+          label={data.props.label}
+          content={data.props.content}
+          active={currIndex === index}
+          onClick={() => this.handleOpen(index)}
+        />
+      ))
+    );
+  }
+
+  renderSingleItem() {
+    const { children } = this.props;
+    const { isOpenSingleItem } = this.state;
+
+    return (
+      <AccordionPanel
+        label={children.props.label}
+        content={children.props.content}
+        active={isOpenSingleItem}
+        onClick={this.handleOpenSingleItem}
+      />
+    );
+  }
+
+  render() {
+    const { children, className } = this.props;
+    const { currIndex, isOpenSingleItem } = this.state;
+
+    return (
+      <AccordionContainer className={className}>
+        <Choose>
+          <When condition={children.length > 1}>
+            {this.renderMultipleItem()}
+          </When>
+          <Otherwise>
+            {this.renderSingleItem()}
+          </Otherwise>
+        </Choose>
+      </AccordionContainer>
+    );
+  }
+}
 
 type Props = {
   children: React$Node,
   className: string,
+};
+
+type State = {
+  currIndex: number,
+  isOpenSingleItem: boolean,
 };
 
 export default Accordion;
