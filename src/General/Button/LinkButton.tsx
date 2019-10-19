619751d114706b946b2fb826f750c6dab0b5479f
franciscrispin <franciscrispingoh@gmail.com>
2019-10-19 14:08:00 +0800

    LinkButton: Pass className prop

--- src/General/Button/LinkButton.tsx
+++ src/General/Button/LinkButton.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
+import classNames from 'classnames';
+
 import { LinkBtn } from '../../Style/General/ButtonStyle';
 
 const LinkButton: React.FunctionComponent<Props> = ({
   children,
-  block,
+  className,
   ...defaultProps
 }) => (
-  <LinkBtn className="aries-linkbtn" block={block} {...defaultProps}>
+  <LinkBtn className={classNames('aries-linkbtn', className)} {...defaultProps}>
     {children}
   </LinkBtn>
 );
