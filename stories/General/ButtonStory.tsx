fd75da9fbe89333ba35483d6fa0a11a376d78521
westwood846 <PapstDonB@Googlemail.com>
2019-10-25 17:01:38 +0800

    Format ButtonStory.tsx

--- stories/General/ButtonStory.tsx
+++ stories/General/ButtonStory.tsx
@@ -109,212 +109,238 @@ const linkButtonProps = {
 
 const ButtonStories = () => (
   <React.Fragment>
-    <DefaultButtonStory/>
+    <DefaultButtonStory />
     <Divider theme="grey" />
-    <PrimaryButtonStory/>
+    <PrimaryButtonStory />
     <Divider theme="grey" />
-    <SecondaryButtonStory/>
+    <SecondaryButtonStory />
     <Divider theme="grey" />
-    <GhostButtonStory/>
+    <GhostButtonStory />
     <Divider theme="grey" />
-    <LinkButtonStory/>
+    <LinkButtonStory />
   </React.Fragment>
 );
 
 const DefaultButtonStory = () => (
   <StorybookComponent
-  title="Button"
-  code="import { Button } from 'glints-aries'"
-  propsObject={defaultButtonProps}
-  usage={`<Button
+    title="Button"
+    code="import { Button } from 'glints-aries'"
+    propsObject={defaultButtonProps}
+    usage={`<Button
 theme="blue"
 >
 Default
 </Button>`}
->
-  <div style={{ marginBottom: '2em' }}>
-    <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-      Default Button
-    </Heading>
-    <ButtonRow>
-      <ButtonContainer>
-        <Button theme={Theme.WHITE} onClick={action('Default Button')}>
-          Default
-        </Button>
-      </ButtonContainer>
-      <ButtonContainer>
-        <Button theme={Theme.RED}  onClick={action('Default Button')}>
-          Red
-        </Button>
-      </ButtonContainer>
-    </ButtonRow>
-    <ButtonRow>
-      <ButtonContainer>
-        <Button theme={Theme.BLUE} onClick={action('Default Button')}>
-          Blue
-        </Button>
-      </ButtonContainer>
-      <ButtonContainer>
-        <Button theme={Theme.YELLOW} onClick={action('Default Button')}>
-          Yellow
-        </Button>
-      </ButtonContainer>
-    </ButtonRow>
-    <ButtonRow>
-      <ButtonContainer>
-        <Button disabled onClick={action('Default Button')}>
-          Disabled
-        </Button>
-      </ButtonContainer>
-      <BlockButtonContainer>
-        <Button theme={Theme.YELLOW} block onClick={action('Default Button')}>
-          Block
-        </Button>
-      </BlockButtonContainer>
-    </ButtonRow>
-  </div>
-</StorybookComponent>
-)
+  >
+    <div style={{ marginBottom: '2em' }}>
+      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
+        Default Button
+      </Heading>
+      <ButtonRow>
+        <ButtonContainer>
+          <Button theme={Theme.WHITE} onClick={action('Default Button')}>
+            Default
+          </Button>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button theme={Theme.RED} onClick={action('Default Button')}>
+            Red
+          </Button>
+        </ButtonContainer>
+      </ButtonRow>
+      <ButtonRow>
+        <ButtonContainer>
+          <Button theme={Theme.BLUE} onClick={action('Default Button')}>
+            Blue
+          </Button>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button theme={Theme.YELLOW} onClick={action('Default Button')}>
+            Yellow
+          </Button>
+        </ButtonContainer>
+      </ButtonRow>
+      <ButtonRow>
+        <ButtonContainer>
+          <Button disabled onClick={action('Default Button')}>
+            Disabled
+          </Button>
+        </ButtonContainer>
+        <BlockButtonContainer>
+          <Button theme={Theme.YELLOW} block onClick={action('Default Button')}>
+            Block
+          </Button>
+        </BlockButtonContainer>
+      </ButtonRow>
+    </div>
+  </StorybookComponent>
+);
 
 const PrimaryButtonStory = () => (
   <StorybookComponent
-  propsObject={primaryButtonProps}
-  usage={`<Button
+    propsObject={primaryButtonProps}
+    usage={`<Button
 variant='primary'
 theme='yellow'
 >
 Primary
 </Button>`}
->
-  <div style={{ marginBottom: '2em' }}>
-    <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-      Primary Button
-    </Heading>
-    <ButtonRow>
-      <ButtonContainer>
-        <Button variant={Variant.PRIMARY} theme={Theme.YELLOW} onClick={action('Primary Button')}>
-          Yellow
-        </Button>
-      </ButtonContainer>
-      <ButtonContainer>
-        <Button variant={Variant.PRIMARY} theme={Theme.RED}  onClick={action('Primary Button')}>
-          Red
-        </Button>
-      </ButtonContainer>
-    </ButtonRow>
-    <ButtonRow>
-      <ButtonContainer>
-        <Button variant={Variant.PRIMARY} theme={Theme.BLUE} onClick={action('Primary Button')}>
-          Blue
-        </Button>
-      </ButtonContainer>
-      <ButtonContainer>
-        <Button variant={Variant.PRIMARY} theme={Theme.BLUE_RED}  onClick={action('Primary Button')}>
-          Blue-Red
-        </Button>
-      </ButtonContainer>
-    </ButtonRow>
-    <ButtonRow>
-      <BlockButtonContainer>
-        <Button variant={Variant.PRIMARY} theme={Theme.YELLOW} block onClick={action('Primary Button')}>
-          Block
-        </Button>
-      </BlockButtonContainer>
-      <ButtonContainer>
-        <Button variant={Variant.PRIMARY} disabled onClick={action('Primary Button')}>
-          Disabled
-        </Button>
-      </ButtonContainer>
-    </ButtonRow>
-  </div>
-</StorybookComponent>
-)
+  >
+    <div style={{ marginBottom: '2em' }}>
+      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
+        Primary Button
+      </Heading>
+      <ButtonRow>
+        <ButtonContainer>
+          <Button
+            variant={Variant.PRIMARY}
+            theme={Theme.YELLOW}
+            onClick={action('Primary Button')}
+          >
+            Yellow
+          </Button>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button
+            variant={Variant.PRIMARY}
+            theme={Theme.RED}
+            onClick={action('Primary Button')}
+          >
+            Red
+          </Button>
+        </ButtonContainer>
+      </ButtonRow>
+      <ButtonRow>
+        <ButtonContainer>
+          <Button
+            variant={Variant.PRIMARY}
+            theme={Theme.BLUE}
+            onClick={action('Primary Button')}
+          >
+            Blue
+          </Button>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button
+            variant={Variant.PRIMARY}
+            theme={Theme.BLUE_RED}
+            onClick={action('Primary Button')}
+          >
+            Blue-Red
+          </Button>
+        </ButtonContainer>
+      </ButtonRow>
+      <ButtonRow>
+        <BlockButtonContainer>
+          <Button
+            variant={Variant.PRIMARY}
+            theme={Theme.YELLOW}
+            block
+            onClick={action('Primary Button')}
+          >
+            Block
+          </Button>
+        </BlockButtonContainer>
+        <ButtonContainer>
+          <Button
+            variant={Variant.PRIMARY}
+            disabled
+            onClick={action('Primary Button')}
+          >
+            Disabled
+          </Button>
+        </ButtonContainer>
+      </ButtonRow>
+    </div>
+  </StorybookComponent>
+);
 
 const SecondaryButtonStory = () => (
   <StorybookComponent
-      propsObject={secondaryButtonProps}
-      usage={`<Button
+    propsObject={secondaryButtonProps}
+    usage={`<Button
   variant="secondary"
 >
   Secondary
 </Button>`}
-    >
-      <div style={{ marginBottom: '2em' }}>
-        <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-          Secondary Button
-        </Heading>
-        <Button
-          variant={Variant.SECONDARY}
-          onClick={action('Secondary Button')}
-        >
-          Secondary
-        </Button>
-      </div>
-    </StorybookComponent>
-)
+  >
+    <div style={{ marginBottom: '2em' }}>
+      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
+        Secondary Button
+      </Heading>
+      <Button variant={Variant.SECONDARY} onClick={action('Secondary Button')}>
+        Secondary
+      </Button>
+    </div>
+  </StorybookComponent>
+);
 
 const GhostButtonStory = () => (
   <StorybookComponent
-      propsObject={ghostButtonProps}
-      usage={`<Button
+    propsObject={ghostButtonProps}
+    usage={`<Button
   variant="ghost"
   theme="blue"
 >
   Ghost
 </Button>`}
-    >
-      <div style={{ marginBottom: '2em' }}>
-        <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-          Ghost Button
-        </Heading>
-        <Button
-          variant={Variant.GHOST}
-          theme={Theme.BLUE}
-          onClick={action('Ghost Button')}
-        >
-          Ghost
-        </Button>
-      </div>
-    </StorybookComponent>
-)
+  >
+    <div style={{ marginBottom: '2em' }}>
+      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
+        Ghost Button
+      </Heading>
+      <Button
+        variant={Variant.GHOST}
+        theme={Theme.BLUE}
+        onClick={action('Ghost Button')}
+      >
+        Ghost
+      </Button>
+    </div>
+  </StorybookComponent>
+);
 
 const LinkButtonStory = () => (
   <StorybookComponent
-      propsObject={linkButtonProps}
-      usage={`<Button
+    propsObject={linkButtonProps}
+    usage={`<Button
   variant="link"
   theme="blue"
 >
   Link
 </Button>`}
-    >
-      <div style={{ marginBottom: '2em' }}>
-        <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-          Link Button
-        </Heading>
-        <Button variant={Variant.LINK} onClick={action('Link Button')}>
-          Link
-        </Button>
-      </div>
-    </StorybookComponent>
-)
+  >
+    <div style={{ marginBottom: '2em' }}>
+      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
+        Link Button
+      </Heading>
+      <Button variant={Variant.LINK} onClick={action('Link Button')}>
+        Link
+      </Button>
+    </div>
+  </StorybookComponent>
+);
 
-const ButtonRow = ({children}) => (
-  <div style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '2em' }}>
+const ButtonRow = ({ children }) => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'space-around',
+      marginBottom: '2em',
+    }}
+  >
     {children}
   </div>
-)
+);
 
-const BlockButtonContainer = ({children}) => (
-  <div style={{ flex: '0 0 40%' }}>
-    {children}
-  </div>
-)
+const BlockButtonContainer = ({ children }) => (
+  <div style={{ flex: '0 0 40%' }}>{children}</div>
+);
 
-const ButtonContainer = ({children}) => (
+const ButtonContainer = ({ children }) => (
   <div style={{ flex: '0 0 40%', display: 'flex', justifyContent: 'center' }}>
     {children}
   </div>
-)
+);
 
 export default ButtonStories;
