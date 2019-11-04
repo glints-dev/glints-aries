da3a195e9049058982a01626a84124aca95fecb0
franciscrispin <franciscrispingoh@gmail.com>
2019-11-04 14:17:45 +0800

    Paragraph.test: Fix typescript error

--- src/General/Typography/Paragraph.test.tsx
+++ src/General/Typography/Paragraph.test.tsx
@@ -15,7 +15,7 @@ const paragraphText =
   'As the first sign in the zodiac, the presence of Aries always marks the beginning of something energetic and turbulent. They are continuously looking for dynamic, speed and competition, always being the first in everything - from work to social gatherings.';
 
 describe('displays the correct text and font-size for each variant:', () => {
-  const variants: paragraphType[] = Object.values(PARAGRAPH_VARIANTS);
+  const variants = Object.values(PARAGRAPH_VARIANTS) as paragraphType[];
   variants.forEach(variant => {
     const fontSize = PARAGRAPH_FONT_SIZES[variant];
     it(`${variant}: ${fontSize}px`, () => {
