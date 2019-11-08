f359c97f6915f2fd2e6e3ea222c9fdca16350f84
ross <ross@glints.com>
2019-11-08 18:37:40 +0800

    Feat: add es module

---
+++ .babelrc.js
@@ -0,0 +1,34 @@
+const isEs = process.env.BABEL_ENV === 'es';
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-typescript',
+      {
+        isTSX: true,
+        allExtensions: true,
+      },
+    ],
+    [
+      '@babel/preset-env',
+      {
+        modules: isEs ? false : 'commonjs',
+        targets: {
+          browsers: ['> 99.5%'],
+        },
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        regenerator: true,
+      },
+    ],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    'syntax-dynamic-import',
+  ],
+};
