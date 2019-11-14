2bf81e37535c6cabb42f747033409536e511751f
ross <ross@glints.com>
2019-11-14 11:53:39 +0800

    Feat: Support legacy icon usage way but have a warning message

---
+++ src/General/Icon/LegacyIcon.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { camelCase, upperFirst } from 'lodash';
+import { ICONS } from '../../Utils/Icon/IconLibrary';
+
+const formatWarning = (name: string) => {
+  const camelCaseName = upperFirst(camelCase(name));
+
+  return `Warning: <Icon name="${name}" /> will be deprecated in a future release.\n\nPlease use <${camelCaseName}Icon /> to instead. See http://aries.glints.design/?path=/story/general--icon for details.`;
+};
+
+const LegacyIcon: React.FunctionComponent<Props> = props => {
+  const { name, color } = props;
+  const iconName = name.replace(/-/g, '_');
+
+  if (typeof console !== 'undefined') {
+    console.warn(formatWarning(name));
+  }
+
+  return (
+    <svg width="1em" height="1em" fill={color} viewBox="0 0 100 100">
+      {(ICONS as any)[iconName.toUpperCase()]}
+    </svg>
+  );
+};
+
+export interface Props {
+  name: string;
+  color?: string;
+}
+
+export default LegacyIcon;
