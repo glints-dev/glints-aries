17dc6022bb8021040659201a19e888743e104c3f
franciscrispin <franciscrispingoh@gmail.com>
2019-12-22 19:08:31 +0800

    SwipeableItem: Pass className prop to component to allow for extending styles using styled-components

--- src/Display/Swipeable/SwipeableItem.tsx
+++ src/Display/Swipeable/SwipeableItem.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import { SwipeableItemWrapper } from '../../Style/Display/SwipeableStyle';
 
 const SwipeableItem: React.FunctionComponent<Props> = props => {
   const { children, className, ...defaultProps } = props;
 
   return (
-    <SwipeableItemWrapper className="swipeable-itemwrapper" {...defaultProps}>
+    <SwipeableItemWrapper
+      className={classNames('swipeable-itemwrapper', className)}
+      {...defaultProps}
+    >
       {children}
     </SwipeableItemWrapper>
   );
