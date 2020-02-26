48aaac9e199b140f221258f21853d88087720467
Tony Ling <tony@glints.com>
2020-02-26 17:23:16 +0800

    .babelrc: added babel-plugin-styled-components with ssr support

--- .babelrc.js
+++ .babelrc.js
@@ -30,5 +30,11 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
+    [
+      'babel-plugin-styled-components',
+      {
+        ssr: true,
+      },
+    ],
   ],
 };
