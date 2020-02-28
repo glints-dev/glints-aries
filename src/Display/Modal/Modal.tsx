7a62e7dd763756bbc63a34995e73c2e2a755b190
westwood846 <PapstDonB@Googlemail.com>
2020-02-28 15:32:47 +0800

    Modal: Convert to functional component
    Simplifies the internal state management too.

--- src/Display/Modal/Modal.tsx
+++ src/Display/Modal/Modal.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 
 import { CloseIcon } from '../../General/Icon/components';
-import { escEvent } from '../../Utils/DomUtils';
+import { escEvent as createEscapeKeyEventListener } from '../../Utils/DomUtils';
 
 import {
   ModalContainer,
@@ -13,144 +13,115 @@ import {
   ModalFooter,
 } from '../../Style/Display/ModalStyle';
 
-class Modal extends React.Component<Props, State> {
-  modalContentAreaRef: React.RefObject<HTMLDivElement>;
-  mouseDownTarget: HTMLElement;
-  escEvent: (e: KeyboardEvent) => any;
+const Modal = (props: Props) => {
+  const {
+    isVisible,
+    title,
+    onClose,
+    children,
+    className,
+    hideContentArea,
+    centering,
+    removeAnimation,
+    footer,
+    size,
+    hideHeader,
+    ...restProps
+  } = props;
 
-  state = {
-    isOpen: false,
-  };
+  const modalContentAreaRef = React.useRef(null);
 
-  constructor(props: Props) {
-    super(props);
-    this.modalContentAreaRef = React.createRef();
-  }
+  React.useEffect(() => {
+    if (isVisible) modalContentAreaRef.current.focus();
+  }, [isVisible]);
 
-  registerEventEscapeListener() {
-    if (!this.props.isVisible) return;
-    this.escEvent = escEvent(this.props.onClose);
-    document.addEventListener('keydown', this.escEvent, false);
-  }
+  React.useEffect(() => {
+    const escapeKeyEventListener = createEscapeKeyEventListener(() => {
+      if (isVisible) onClose();
+    });
 
-  componentDidMount() {
-    this.registerEventEscapeListener();
-  }
+    document.addEventListener('keydown', escapeKeyEventListener, false);
 
-  static getDerivedStateFromProps(nextProps: Props, prevState: State) {
-    if (nextProps.isVisible && !prevState.isOpen) {
-      document.body.style.overflow = 'hidden';
-      return { isOpen: true };
-    }
-
-    if (!nextProps.isVisible && prevState.isOpen) {
-      document.body.removeAttribute('style');
-      return { isOpen: false };
-    }
-    return null;
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    const { isVisible } = this.props;
-    if (!prevProps.isVisible && isVisible) {
-      this.modalContentAreaRef.current.focus();
-      this.registerEventEscapeListener();
-    }
-    if (prevProps.isVisible && !isVisible) {
-      this.removeEscapeEventListener();
-    }
-  }
-
-  removeEscapeEventListener() {
-    document.removeEventListener('keydown', this.escEvent, false);
-  }
-
-  componentWillUnmount() {
-    this.removeEscapeEventListener();
-    document.body.removeAttribute('style');
-  }
+    // This function will be called on unmount and _before_ this effect is
+    // re-executed beause its dependencies changed. This gives us the chance
+    // to clean up the existing escape key event listener.
+    return () =>
+      document.removeEventListener('keydown', escapeKeyEventListener, false);
+  }, [isVisible, onClose]);
 
   // To prevent the modal from closing
   // when a mousedown event occurs inside the ModalContentArea
   // but the subsequent mouseup event occurs outside
-  handleMouseDown = (e: React.MouseEvent) => {
-    const element = e.target as HTMLElement;
-    this.mouseDownTarget = element;
-  };
+  const [mouseDownTarget, setMouseDownTarget] = React.useState<HTMLElement>(
+    null
+  );
 
-  handleClick = (e: React.MouseEvent) => {
-    const element = e.target as HTMLElement;
-    if (this.mouseDownTarget === element) {
-      this.props.onClose();
-    }
-  };
+  const handleMouseDown = React.useCallback(
+    (e: React.MouseEvent) => {
+      const element = e.target as HTMLElement;
+      setMouseDownTarget(element);
+    },
+    [mouseDownTarget]
+  );
 
-  render() {
-    const {
-      title,
-      onClose,
-      children,
-      className,
-      hideContentArea,
-      centering,
-      removeAnimation,
-      footer,
-      size,
-      hideHeader,
-      ...restProps
-    } = this.props;
+  const handleClick = React.useCallback(
+    (e: React.MouseEvent) => {
+      const element = e.target as HTMLElement;
+      if (mouseDownTarget === element) {
+        onClose();
+      }
+    },
+    [mouseDownTarget, onClose]
+  );
 
-    const { isOpen } = this.state;
-
-    return (
-      <ModalContainer
-        data-testid="modal-container"
-        className={classNames('aries-modal', className)}
-        centering={centering}
-        isOpen={isOpen}
-        removeAnimation={removeAnimation}
-        onMouseDown={this.handleMouseDown}
-        onClick={this.handleClick}
-      >
-        <ModalDialog className="modal-dialog">
-          <ModalContentArea
-            className="modal-content"
-            role="dialog"
-            aria-modal="true"
-            hideContentArea={hideContentArea}
-            centering={centering}
-            onClick={e => e.stopPropagation()}
-            tabIndex={0}
-            isOpen={isOpen}
-            removeAnimation={removeAnimation}
-            size={size}
-            ref={this.modalContentAreaRef}
-            {...restProps}
-          >
-            {!hideHeader && (
-              <ModalHeader className="modal-header">
-                <h3>{title}</h3>
-                <button
-                  data-testid="close-button"
-                  type="button"
-                  onClick={onClose}
-                >
-                  <CloseIcon color={hideContentArea ? 'white' : 'grey'} />
-                </button>
-              </ModalHeader>
-            )}
-            <ModalBody className="modal-body" hideContentArea={hideContentArea}>
-              {children}
-            </ModalBody>
-            {footer !== undefined && (
-              <ModalFooter className="modal-footer">{footer}</ModalFooter>
-            )}
-          </ModalContentArea>
-        </ModalDialog>
-      </ModalContainer>
-    );
-  }
-}
+  return (
+    <ModalContainer
+      data-testid="modal-container"
+      className={classNames('aries-modal', className)}
+      centering={centering}
+      isOpen={isVisible}
+      removeAnimation={removeAnimation}
+      onMouseDown={handleMouseDown}
+      onClick={handleClick}
+    >
+      <ModalDialog className="modal-dialog">
+        <ModalContentArea
+          className="modal-content"
+          role="dialog"
+          aria-modal="true"
+          hideContentArea={hideContentArea}
+          centering={centering}
+          onClick={e => e.stopPropagation()}
+          tabIndex={0}
+          isOpen={isVisible}
+          removeAnimation={removeAnimation}
+          size={size}
+          ref={modalContentAreaRef}
+          {...restProps}
+        >
+          {!hideHeader && (
+            <ModalHeader className="modal-header">
+              <h3>{title}</h3>
+              <button
+                data-testid="close-button"
+                type="button"
+                onClick={onClose}
+              >
+                <CloseIcon color={hideContentArea ? 'white' : 'grey'} />
+              </button>
+            </ModalHeader>
+          )}
+          <ModalBody className="modal-body" hideContentArea={hideContentArea}>
+            {children}
+          </ModalBody>
+          {footer !== undefined && (
+            <ModalFooter className="modal-footer">{footer}</ModalFooter>
+          )}
+        </ModalContentArea>
+      </ModalDialog>
+    </ModalContainer>
+  );
+};
 
 export type sizeType = 's' | 'm' | 'l' | 'xl';
 
@@ -166,10 +137,6 @@ interface Props
   footer?: React.ReactElement[];
   size?: sizeType;
   hideHeader?: boolean;
-}
-
-interface State {
-  isOpen: boolean;
 }
 
 export default Modal;
