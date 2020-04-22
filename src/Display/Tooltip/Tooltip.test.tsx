ca792ad37f8da6a04d7a1e268b53e66e0f6b1508
ting <tingtingdisco@gmail.com>
2020-04-22 23:34:59 +0800

    Tooltip: Add tests

---
+++ src/Display/Tooltip/Tooltip.test.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import 'jest-styled-components';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render } from '@testing-library/react';
+
+import Tooltip from './Tooltip';
+
+describe('<Tooltip/> snapshots', () => {
+  const TooltipComponent = () => (
+    <Tooltip
+      text="mock text"
+      position="right"
+      classes={{
+        container: 'container',
+        content: 'content',
+        message: 'message',
+      }}
+    >
+      <span>mock content</span>
+    </Tooltip>
+  );
+
+  test('should match snapshot when Tooltip is hidden', () => {
+    const { asFragment } = render(<TooltipComponent />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('should match snapshot when Tooltip is shown', () => {
+    const { asFragment, queryByText } = render(<TooltipComponent />);
+    const element = queryByText('mock content');
+    fireEvent.mouseOver(element);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('<Tooltip/> touch event', () => {
+  const TooltipComponent = () => (
+    <React.Fragment>
+      <div>touch outside</div>
+      <Tooltip text="mock text">
+        <span>mock content</span>
+      </Tooltip>
+    </React.Fragment>
+  );
+
+  test('touchstart event should show and hide Tooltip', () => {
+    const { queryByText } = render(<TooltipComponent />);
+    expect(queryByText('mock text')).not.toBeInTheDocument();
+
+    const element = queryByText('mock content');
+    fireEvent.touchStart(element);
+    expect(queryByText('mock text')).toBeInTheDocument();
+
+    fireEvent.touchStart(element);
+    expect(queryByText('mock text')).not.toBeInTheDocument();
+  });
+
+  test('touch outside of Tooltip should hide Tooltip', () => {
+    const { queryByText } = render(<TooltipComponent />);
+    expect(queryByText('mock text')).not.toBeInTheDocument();
+
+    const element = queryByText('mock content');
+    fireEvent.touchStart(element);
+    expect(queryByText('mock text')).toBeInTheDocument();
+
+    const outside = queryByText('touch outside');
+    fireEvent.touchStart(outside);
+    expect(queryByText('mock text')).not.toBeInTheDocument();
+  });
+
+  test('touch outside should register and unregister hideTooltipIfTouchOutside callback', () => {
+    const addEventListenerSpy = jest.spyOn(document, 'addEventListener');
+    const removeEventListenerSpy = jest.spyOn(document, 'removeEventListener');
+    const useCallbackSpy = jest.spyOn(React, 'useCallback');
+
+    const { queryByText } = render(<TooltipComponent />);
+    const element = queryByText('mock content');
+    fireEvent.touchStart(element);
+    expect(addEventListenerSpy.mock.calls[0][1]).toEqual(
+      useCallbackSpy.mock.results[0].value
+    );
+
+    const outside = queryByText('touch outside');
+    fireEvent.touchStart(outside);
+
+    expect(removeEventListenerSpy.mock.calls[0][1]).toEqual(
+      useCallbackSpy.mock.results[0].value
+    );
+
+    jest.clearAllMocks();
+  });
+});
