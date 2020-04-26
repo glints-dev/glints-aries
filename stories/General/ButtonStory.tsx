cd6ffb279dbc47c2aaa5549c781cac912e2fb565
ross <ross@glints.com>
2020-04-26 19:17:20 +0800

    Button: remove legacy stories

--- stories/General/ButtonStory.tsx
+++ stories/General/ButtonStory.tsx
@@ -15,103 +15,6 @@ import {
 
 import { Variant, Theme } from '../../src/Utils/StyleConfig';
 
-const blockProp = {
-  name: 'block',
-  type: 'boolean',
-  defaultValue: <code>false</code>,
-  possibleValue: <code>true | false</code>,
-  require: 'no',
-  description: 'Make the button fit to its parent width.',
-};
-
-const smallProp = {
-  name: 'small',
-  type: 'boolean',
-  defaultValue: <code>false</code>,
-  possibleValue: <code>true | false</code>,
-  require: 'no',
-  description: 'Sets Button to small version.',
-};
-
-const buttonProps = [
-  {
-    name: 'variant',
-    type: 'string',
-    defaultValue: 'default',
-    possibleValue: <code>default | primary | secondary | ghost | link</code>,
-    require: 'no',
-    description: 'Sets the Default Button.',
-  },
-  {
-    name: 'disabled',
-    type: 'boolean',
-    defaultValue: <code>false</code>,
-    possibleValue: <code>true | false</code>,
-    require: 'no',
-    description: 'Sets Button to disable state.',
-  },
-  blockProp,
-  smallProp,
-  {
-    name: 'tag',
-    type: 'React.ElementType',
-    defaultValue: <code>button</code>,
-    possibleValue: 'any valid html tag e.g. "a"',
-    require: 'no',
-    description: 'Changes the tag with which the button will render.',
-  },
-];
-
-const removeHoverEffectProp = {
-  name: 'removeHoverEffect',
-  type: 'boolean',
-  defaultValue: <code>false</code>,
-  possibleValue: <code>true | false</code>,
-  require: 'no',
-  description: "Removes Button's effect when hovered",
-};
-
-const defaultButtonProps = {
-  'Default Button': [
-    {
-      name: 'theme',
-      type: 'string',
-      defaultValue: <code>white</code>,
-      possibleValue: <code>white | red | blue | yellow</code>,
-      require: 'no',
-      description: "Sets the Button's theme",
-    },
-    ...buttonProps,
-    removeHoverEffectProp,
-  ],
-};
-
-const primaryButtonProps = {
-  'Primary Button': [
-    {
-      name: 'theme',
-      type: 'string',
-      defaultValue: <code>yellow</code>,
-      possibleValue: <code>red | blue | blue-red | yellow</code>,
-      require: 'no',
-      description: "Sets the Button's theme",
-    },
-    ...buttonProps,
-  ],
-};
-
-const secondaryButtonProps = {
-  'Secondary Button': buttonProps,
-};
-
-const ghostButtonProps = {
-  'Ghost Button': [...buttonProps, removeHoverEffectProp],
-};
-
-const linkButtonProps = {
-  'Link Button': [blockProp],
-};
-
 const ButtonStories = () => (
   <React.Fragment>
     <ButtonVariantStory />
@@ -127,14 +30,6 @@ const ButtonStories = () => (
     <ButtonWithTagStory />
     <Divider theme="grey" />
     <ButtonRemoveHoverEffectStory />
-    <Divider theme="grey" />
-    <DefaultButtonStory />
-    <Divider theme="grey" />
-    <PrimaryButtonStory />
-    <Divider theme="grey" />
-    <GhostButtonStory />
-    <Divider theme="grey" />
-    <LinkButtonStory />
     <Divider theme="grey" />
     <SecondaryButtonStory />
   </React.Fragment>
@@ -252,7 +147,24 @@ const ButtonSizeStory = () => {
 <Button variant="${Variant.GHOST}" block>Block</Button>
 `;
   const propsObject = {
-    'Default Button, Primary Button, Ghost Button': [smallProp, blockProp],
+    'Default Button, Primary Button, Ghost Button': [
+      {
+        name: 'small',
+        type: 'boolean',
+        defaultValue: <code>false</code>,
+        possibleValue: <code>true | false</code>,
+        require: 'no',
+        description: 'Sets Button to small version.',
+      },
+      {
+        name: 'block',
+        type: 'boolean',
+        defaultValue: <code>false</code>,
+        possibleValue: <code>true | false</code>,
+        require: 'no',
+        description: 'Make the button fit to its parent width.',
+      },
+    ],
   };
 
   return (
@@ -469,139 +381,8 @@ const ButtonRemoveHoverEffectStory = () => {
   );
 };
 
-const DefaultButtonStory = () => (
-  <StorybookComponent
-    title="Button"
-    code="import { Button } from 'glints-aries'"
-    propsObject={defaultButtonProps}
-    usage={`<Button
-theme="blue"
->
-Default
-</Button>`}
-  >
-    <div style={{ marginBottom: '2em' }}>
-      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-        Default Button
-      </Heading>
-      <ButtonRow>
-        <ButtonContainer>
-          <Button theme={Theme.WHITE} onClick={() => null}>
-            Default
-          </Button>
-        </ButtonContainer>
-        <ButtonContainer>
-          <Button theme={Theme.RED} onClick={() => null}>
-            Red
-          </Button>
-        </ButtonContainer>
-      </ButtonRow>
-      <ButtonRow>
-        <ButtonContainer>
-          <Button theme={Theme.BLUE} onClick={() => null}>
-            Blue
-          </Button>
-        </ButtonContainer>
-        <ButtonContainer>
-          <Button theme={Theme.YELLOW} onClick={() => null}>
-            Yellow
-          </Button>
-        </ButtonContainer>
-      </ButtonRow>
-      <ButtonRow>
-        <ButtonContainer>
-          <Button disabled onClick={() => null}>
-            Disabled
-          </Button>
-        </ButtonContainer>
-        <BlockButtonContainer>
-          <Button theme={Theme.YELLOW} block onClick={() => null}>
-            Block
-          </Button>
-        </BlockButtonContainer>
-      </ButtonRow>
-    </div>
-  </StorybookComponent>
-);
-
-const PrimaryButtonStory = () => (
-  <StorybookComponent
-    propsObject={primaryButtonProps}
-    usage={`<Button
-variant='primary'
-theme='yellow'
->
-Primary
-</Button>`}
-  >
-    <div style={{ marginBottom: '2em' }}>
-      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-        Primary Button
-      </Heading>
-      <ButtonRow>
-        <ButtonContainer>
-          <Button
-            variant={Variant.PRIMARY}
-            theme={Theme.YELLOW}
-            onClick={() => null}
-          >
-            Yellow
-          </Button>
-        </ButtonContainer>
-        <ButtonContainer>
-          <Button
-            variant={Variant.PRIMARY}
-            theme={Theme.RED}
-            onClick={() => null}
-          >
-            Red
-          </Button>
-        </ButtonContainer>
-      </ButtonRow>
-      <ButtonRow>
-        <ButtonContainer>
-          <Button
-            variant={Variant.PRIMARY}
-            theme={Theme.BLUE}
-            onClick={() => null}
-          >
-            Blue
-          </Button>
-        </ButtonContainer>
-        <ButtonContainer>
-          <Button
-            variant={Variant.PRIMARY}
-            theme={Theme.BLUE_RED}
-            onClick={() => null}
-          >
-            Blue-Red
-          </Button>
-        </ButtonContainer>
-      </ButtonRow>
-      <ButtonRow>
-        <BlockButtonContainer>
-          <Button
-            variant={Variant.PRIMARY}
-            theme={Theme.YELLOW}
-            block
-            onClick={() => null}
-          >
-            Block
-          </Button>
-        </BlockButtonContainer>
-        <ButtonContainer>
-          <Button variant={Variant.PRIMARY} disabled onClick={() => null}>
-            Disabled
-          </Button>
-        </ButtonContainer>
-      </ButtonRow>
-    </div>
-  </StorybookComponent>
-);
-
 const SecondaryButtonStory = () => (
   <StorybookComponent
-    propsObject={secondaryButtonProps}
     usage={`<Button
   variant="secondary"
 >
@@ -619,62 +400,6 @@ const SecondaryButtonStory = () => (
   </StorybookComponent>
 );
 
-const GhostButtonStory = () => (
-  <StorybookComponent
-    propsObject={ghostButtonProps}
-    usage={`<Button
-  variant="ghost"
-  theme="blue"
->
-  Ghost
-</Button>`}
-  >
-    <div style={{ marginBottom: '2em' }}>
-      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-        Ghost Button
-      </Heading>
-      <Button variant={Variant.GHOST} theme={Theme.BLUE} onClick={() => null}>
-        Ghost
-      </Button>
-    </div>
-  </StorybookComponent>
-);
-
-const LinkButtonStory = () => (
-  <StorybookComponent
-    propsObject={linkButtonProps}
-    usage={`<Button
-  variant="link"
-  theme="blue"
->
-  Link
-</Button>`}
-  >
-    <div style={{ marginBottom: '2em' }}>
-      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
-        Link Button
-      </Heading>
-      <Button variant={Variant.LINK} onClick={() => null}>
-        Link
-      </Button>
-    </div>
-  </StorybookComponent>
-);
-
-// const ButtonRow: React.FunctionComponent = ({ children }) => (
-//   <div
-//     style={{
-//       display: 'flex',
-//       flexDirection: 'row',
-//       alignItems: 'center',
-//       marginTop: '15px',
-//       marginBottom: '15px',
-//     }}
-//   >
-//     {children}
-//   </div>
-// );
-
 const ButtonRow = styled.div`
   display: flex;
   flex-direction: row;
@@ -689,12 +414,6 @@ const ButtonRow = styled.div`
 
 const BlockButtonContainer: React.FunctionComponent = ({ children }) => (
   <div style={{ flex: '0 0 40%' }}>{children}</div>
-);
-
-const ButtonContainer: React.FunctionComponent = ({ children }) => (
-  <div style={{ flex: '0 0 40%', display: 'flex', justifyContent: 'center' }}>
-    {children}
-  </div>
 );
 
 export default ButtonStories;
