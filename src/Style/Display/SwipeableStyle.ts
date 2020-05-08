c2c9e2e344799813f209f3875d588a0ace902f01
ross <ross@glints.com>
2020-05-08 23:02:37 +0800

    Swipeable: fix style lint

--- src/Style/Display/SwipeableStyle.ts
+++ src/Style/Display/SwipeableStyle.ts
@@ -10,7 +10,7 @@ export const SwipeableContainer = styled.div`
 
   /* make the horizontal scrollbar visible on desktop */
   &::-webkit-scrollbar {
-    -webkit-appearance: none;
+    appearance: none;
     height: 7px;
   }
 
