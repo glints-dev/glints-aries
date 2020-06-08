c8cbbe2ad53722fa0119619a2b64976af82e976c
westwood846 <PapstDonB@Googlemail.com>
2020-06-08 09:04:49 +0800

    Brand: Convert to functional component

--- src/General/Brand/Brand.tsx
+++ src/General/Brand/Brand.tsx
@@ -7,10 +7,17 @@ import { BrandContainer, BrandImage } from '../../Style/General/BrandStyle';
 const GlintsBlack = require('../../../assets/image/glints-logo-black.svg');
 const GlintsWhite = require('../../../assets/image/glints-logo-white.svg');
 
-class Brand extends React.Component<Props> {
-  handleRightClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const { rightClickURL, onContextMenu } = this.props;
-
+const Brand = ({
+  alt,
+  asset,
+  className,
+  rightClickURL,
+  onContextMenu,
+  ...defaultProps
+}: Props) => {
+  const handleRightClick = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
     if (rightClickURL) {
       e.preventDefault();
 
@@ -22,37 +29,33 @@ class Brand extends React.Component<Props> {
     }
   };
 
-  render() {
-    const { alt, asset, className, ...defaultProps } = this.props;
+  let srcAsset = '';
 
-    let srcAsset = '';
-
-    if (asset === 'glints-black') {
-      srcAsset = GlintsBlack;
-    } else if (asset === 'glints-white') {
-      srcAsset = GlintsWhite;
-    } else {
-      srcAsset = asset;
-    }
-
-    return (
-      <BrandContainer
-        className={classNames('aries-brand', className)}
-        role="presentation"
-        tabIndex={0}
-        onContextMenu={this.handleRightClick}
-        {...defaultProps}
-      >
-        <BrandImage
-          className="brand-image"
-          src={srcAsset}
-          alt={alt}
-          tabIndex={-1}
-        />
-      </BrandContainer>
-    );
+  if (asset === 'glints-black') {
+    srcAsset = GlintsBlack;
+  } else if (asset === 'glints-white') {
+    srcAsset = GlintsWhite;
+  } else {
+    srcAsset = asset;
   }
-}
+
+  return (
+    <BrandContainer
+      className={classNames('aries-brand', className)}
+      role="presentation"
+      tabIndex={0}
+      onContextMenu={handleRightClick}
+      {...defaultProps}
+    >
+      <BrandImage
+        className="brand-image"
+        src={srcAsset}
+        alt={alt}
+        tabIndex={-1}
+      />
+    </BrandContainer>
+  );
+};
 
 interface Props extends React.ComponentPropsWithoutRef<typeof BrandContainer> {
   asset: string;
