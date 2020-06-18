558f21dd47efe26dad9c1b7f5e6011eaa1c6e256
westwood846 <PapstDonB@Googlemail.com>
2020-06-18 14:11:27 +0800

    Slider: Update tests

--- src/Display/Slider/Slider.test.tsx
+++ src/Display/Slider/Slider.test.tsx
@@ -1,6 +1,6 @@
 import 'jest-styled-components';
 import '@testing-library/jest-dom/extend-expect';
-import { render, wait, fireEvent } from '@testing-library/react';
+import { render, wait, fireEvent, act } from '@testing-library/react';
 import * as React from 'react';
 
 import Slider, { Props } from './Slider';
@@ -13,6 +13,13 @@ const SliderComponent = (props: SliderComponentProp) => (
     <Slider.Item>c</Slider.Item>
   </Slider>
 );
+
+const mockContainer = (width: number) => {
+  const boundingRect = { width };
+  const element = document.createElement('div');
+  element.getBoundingClientRect = () => boundingRect as DOMRect;
+  return { current: element };
+};
 
 describe('<Slider/> prop className', () => {
   const matchSnapshot = (className: string) => {
@@ -29,18 +36,19 @@ describe('<Slider/> prop className', () => {
 describe('<Slider/> prop initialItem', () => {
   const matchSnapshot = (initialItem: number) => {
     it(`should match snapshot when initialItem ${initialItem} is passed`, async () => {
-      const sliderRef: any = {
-        current: null,
-      };
-      const { rerender, asFragment } = render(
-        <Slider ref={sliderRef} initialItem={initialItem}>
+      const useRefSpy = jest
+        .spyOn(React, 'useRef')
+        .mockReturnValue(mockContainer(400));
+
+      const { asFragment } = render(
+        <Slider initialItem={initialItem}>
           <Slider.Item>a</Slider.Item>
           <Slider.Item>b</Slider.Item>
           <Slider.Item>c</Slider.Item>
         </Slider>
       );
 
-      const index = initialItem > 0 ? initialItem - 1 : 0;
+      const index = initialItem;
       const theThreeNavigationDots = document.querySelectorAll('li');
 
       // wait setState in componentDidMount to set active dot
@@ -50,28 +58,35 @@ describe('<Slider/> prop initialItem', () => {
         ).toBeTruthy();
       });
 
-      // mock getSliderContainerDOMNode to calculate translateX
-      sliderRef.current.getSliderContainerDOMNode = () => ({
-        getBoundingClientRect: () => ({
-          width: 500,
-        }),
-      });
+      expect(asFragment()).toMatchSnapshot();
 
-      // trigger componentWillReceiveProp to recalculate translateX
-      rerender(
-        <Slider ref={sliderRef} initialItem={initialItem}>
+      useRefSpy.mockRestore();
+    });
+  };
+
+  [0, 1, 2].forEach(initialItem => matchSnapshot(initialItem));
+
+  it(`should throw when initialItem is out of bounds`, async () => {
+    // Silence any errors in the console during this test
+    // Otherwise the Slider will throw print his error here, even though the
+    // test passes.
+    const originalError = console.error;
+    console.error = jest.fn();
+
+    const tryToRender = (initialItem: number) =>
+      render(
+        <Slider initialItem={initialItem}>
           <Slider.Item>a</Slider.Item>
           <Slider.Item>b</Slider.Item>
           <Slider.Item>c</Slider.Item>
         </Slider>
       );
 
-      // validate translateX from snapshot
-      expect(asFragment()).toMatchSnapshot();
-    });
-  };
+    expect(() => tryToRender(-1)).toThrow();
+    expect(() => tryToRender(3)).toThrow();
 
-  [0, 1, 2, 3].forEach(initialItem => matchSnapshot(initialItem));
+    console.error = originalError;
+  });
 });
 
 describe('<Slider/> prop fullContent', () => {
@@ -126,7 +141,7 @@ describe('<Slider/> prop autoplay', () => {
     expect(asFragment()).toMatchSnapshot();
 
     // validate autoplay
-    jest.advanceTimersByTime(6000);
+    act(() => jest.advanceTimersByTime(6000));
     expect(theThreeNavigationDots[1].classList.contains('active')).toBeTruthy();
     expect(asFragment()).toMatchSnapshot();
   });
@@ -134,7 +149,7 @@ describe('<Slider/> prop autoplay', () => {
   it(`should go to the first slide after 6000ms when autoplay is truthy and the current slide is the last slide`, () => {
     jest.useFakeTimers();
     const { asFragment } = render(
-      <SliderComponent autoplay={true} initialItem={3} />
+      <SliderComponent autoplay={true} initialItem={2} />
     );
     const theThreeNavigationDots = document.querySelectorAll('li');
     // validate initial active slide
@@ -142,7 +157,7 @@ describe('<Slider/> prop autoplay', () => {
     expect(asFragment()).toMatchSnapshot();
 
     // validate autoplay
-    jest.advanceTimersByTime(6000);
+    act(() => jest.advanceTimersByTime(6000));
     expect(theThreeNavigationDots[0].classList.contains('active')).toBeTruthy();
     expect(asFragment()).toMatchSnapshot();
   });
@@ -156,7 +171,7 @@ describe('<Slider/> prop autoplay', () => {
     expect(asFragment()).toMatchSnapshot();
 
     // validate autoplay
-    jest.advanceTimersByTime(6000);
+    act(() => jest.advanceTimersByTime(6000));
     expect(theThreeNavigationDots[0].classList.contains('active')).toBeTruthy();
     expect(asFragment()).toMatchSnapshot();
   });
@@ -177,19 +192,16 @@ describe('<Slider/> prop afterChange', () => {
     expect(afterChange).not.toBeCalled();
     fireEvent.click(rightArrow);
     expect(afterChange).toHaveBeenCalledTimes(1);
-    expect(afterChange).toHaveBeenLastCalledWith(2);
+    expect(afterChange).toHaveBeenLastCalledWith(1);
     fireEvent.click(firstDot);
     expect(afterChange).toHaveBeenCalledTimes(2);
-    expect(afterChange).toHaveBeenLastCalledWith(1);
-    fireEvent.click(firstDot);
-    expect(afterChange).toHaveBeenCalledTimes(3);
-    expect(afterChange).toHaveBeenLastCalledWith(1);
+    expect(afterChange).toHaveBeenLastCalledWith(0);
   });
 
   test(`when active slide is the middle slide`, () => {
     const afterChange = jest.fn();
     const { queryByTestId } = render(
-      <SliderComponent afterChange={afterChange} initialItem={2} />
+      <SliderComponent afterChange={afterChange} initialItem={1} />
     );
 
     const leftArrow = queryByTestId('slider_left-arrow');
@@ -198,22 +210,19 @@ describe('<Slider/> prop afterChange', () => {
 
     fireEvent.click(leftArrow);
     expect(afterChange).toHaveBeenCalledTimes(1);
-    expect(afterChange).toHaveBeenLastCalledWith(1);
+    expect(afterChange).toHaveBeenLastCalledWith(0);
     fireEvent.click(rightArrow);
     expect(afterChange).toHaveBeenCalledTimes(2);
-    expect(afterChange).toHaveBeenLastCalledWith(2);
+    expect(afterChange).toHaveBeenLastCalledWith(1);
     fireEvent.click(firstDot);
     expect(afterChange).toHaveBeenCalledTimes(3);
-    expect(afterChange).toHaveBeenLastCalledWith(1);
-    fireEvent.click(firstDot);
-    expect(afterChange).toHaveBeenCalledTimes(4);
-    expect(afterChange).toHaveBeenLastCalledWith(1);
+    expect(afterChange).toHaveBeenLastCalledWith(0);
   });
 
   test(`when active slide is the last slide`, () => {
     const afterChange = jest.fn();
     const { queryByTestId } = render(
-      <SliderComponent afterChange={afterChange} initialItem={3} />
+      <SliderComponent afterChange={afterChange} initialItem={2} />
     );
 
     const leftArrow = queryByTestId('slider_left-arrow');
@@ -224,13 +233,11 @@ describe('<Slider/> prop afterChange', () => {
     expect(afterChange).not.toBeCalled();
     fireEvent.click(leftArrow);
     expect(afterChange).toHaveBeenCalledTimes(1);
-    expect(afterChange).toHaveBeenLastCalledWith(2);
+    expect(afterChange).toHaveBeenLastCalledWith(1);
     fireEvent.click(firstDot);
     expect(afterChange).toHaveBeenCalledTimes(2);
-    expect(afterChange).toHaveBeenLastCalledWith(1);
+    expect(afterChange).toHaveBeenLastCalledWith(0);
     fireEvent.click(firstDot);
-    expect(afterChange).toHaveBeenCalledTimes(3);
-    expect(afterChange).toHaveBeenLastCalledWith(1);
   });
 });
 
@@ -249,46 +256,56 @@ describe('<Slider/> keydown event', () => {
 });
 
 describe('<Slider/> unmount', () => {
-  it(`should removeEventListener and clearInterval`, () => {
+  it(`should removeEventListener`, () => {
     const removeEventListenerSpy = jest.spyOn(window, 'removeEventListener');
-    const clearIntervalSpy = jest.spyOn(window, 'clearInterval');
-    const sliderRef: React.LegacyRef<Slider> = { current: null };
     const { unmount } = render(
-      <Slider ref={sliderRef}>
+      <Slider>
         <Slider.Item>a</Slider.Item>
         <Slider.Item>b</Slider.Item>
         <Slider.Item>c</Slider.Item>
       </Slider>
     );
-    const setSize = sliderRef.current.setSize;
-    const interval = sliderRef.current.interval;
     unmount();
-    expect(removeEventListenerSpy).toHaveBeenCalledWith('resize', setSize);
-    expect(clearIntervalSpy).toHaveBeenLastCalledWith(interval);
+    expect(removeEventListenerSpy).toHaveBeenCalledWith(
+      'resize',
+      expect.any(Function)
+    );
+  });
+
+  it(`should clear the autoplay Interval`, () => {
+    const clearIntervalSpy = jest.spyOn(window, 'clearInterval');
+    const { unmount } = render(
+      <Slider autoplay={true}>
+        <Slider.Item>a</Slider.Item>
+        <Slider.Item>b</Slider.Item>
+        <Slider.Item>c</Slider.Item>
+      </Slider>
+    );
+    unmount();
+    expect(clearIntervalSpy).toHaveBeenCalled();
   });
 });
 
-describe('<Slider/> setSize', () => {
-  test(`setSize should be called when resize event is triggered`, () => {
-    const sliderRef: any = {
-      current: null,
-    };
+describe('<Slider/> resizing', () => {
+  it(`should update the translateX value on window resize`, async () => {
+    const useRefSpy = jest
+      .spyOn(React, 'useRef')
+      .mockReturnValue(mockContainer(500));
+
     const { asFragment } = render(
-      <Slider ref={sliderRef} initialItem={2}>
+      <Slider initialItem={1}>
         <Slider.Item>a</Slider.Item>
         <Slider.Item>b</Slider.Item>
         <Slider.Item>c</Slider.Item>
       </Slider>
     );
+    expect(asFragment()).toMatchSnapshot();
 
-    sliderRef.current.getSliderContainerDOMNode = () => ({
-      getBoundingClientRect: () => ({
-        width: 1000,
-      }),
-    });
-
+    useRefSpy.mockReturnValue(mockContainer(400));
     fireEvent(window, new Event('resize'));
     expect(asFragment()).toMatchSnapshot();
+
+    useRefSpy.mockRestore();
   });
 });
 
