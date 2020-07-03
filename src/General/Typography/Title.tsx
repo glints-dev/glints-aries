060f532b1a712d4b5bd429a527cc66a95c0d6095
ritikamotwani <ritika.motwani0712@gmail.com>
2020-07-03 15:31:43 +0530

    Typography: Add upperCase props and stylings to title

--- src/General/Typography/Title.tsx
+++ src/General/Typography/Title.tsx
@@ -11,6 +11,7 @@ const Title: React.FunctionComponent<Props> = (props: Props) => {
     color = SecondaryColor.black,
     ellipsis,
     tag = 'h1',
+    upperCase = false,
     ...restProps
   } = props;
 
@@ -20,6 +21,7 @@ const Title: React.FunctionComponent<Props> = (props: Props) => {
       as={tag}
       color={color}
       ellipsis={ellipsis}
+      upperCase={upperCase}
       {...restProps}
     >
       {children}
@@ -35,6 +37,7 @@ interface Props {
   color?: string;
   ellipsis?: boolean;
   tag?: tagType;
+  upperCase?: boolean;
 }
 
 export default Title;
