3da9dec204ea75d8bab336dbacf66b58c4d1d314
ross <ross@glints.com>
2020-09-01 18:13:39 +0800

    Breadcrumb: add test case to check the last slash is not visible

--- src/Navigation/Breadcrumb/Breadcrumb.test.tsx
+++ src/Navigation/Breadcrumb/Breadcrumb.test.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import 'jest-styled-components';
+import '@testing-library/jest-dom/extend-expect';
 
 import Breadcrumb from './Breadcrumb';
 
@@ -24,5 +26,29 @@ describe('<Breadcrumb>', () => {
 
     const twoItemsSnapshot = renderer.create(twoItems).toJSON();
     expect(twoItemsSnapshot).toMatchSnapshot();
+  });
+
+  it('should show correct slash count', () => {
+    const { queryAllByText } = render(
+      <Breadcrumb>
+        <div>
+          <Breadcrumb.Item>one</Breadcrumb.Item>
+          <Breadcrumb.Item>two</Breadcrumb.Item>
+          <Breadcrumb.Item>three</Breadcrumb.Item>
+        </div>
+      </Breadcrumb>
+    );
+
+    const slashes = queryAllByText('/');
+
+    expect(slashes.length).toEqual(3);
+
+    slashes.forEach((slash, index) => {
+      if (index === slashes.length - 1) {
+        expect(slash).not.toBeVisible();
+      } else {
+        expect(slash).toBeVisible();
+      }
+    });
   });
 });
