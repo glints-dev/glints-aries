5c46dc1a20c7247e229f7db9aa0ed6ab93d7475a
ross <ross@glints.com>
2020-09-21 14:33:53 +0800

    Space: add SpacingScales & Spacing story

---
+++ stories/Layout/SpacingStory.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import StorybookComponent from '../StorybookComponent';
+import { StorybookTable } from '../StorybookTable';
+import { SecondaryColor } from '../../src/Utils/Colors';
+import { SpacingScaleValues, Spacing } from '../../src/Layout/Spacing';
+
+type Data = {
+  value: number;
+  usage: string;
+};
+
+const dataSource: Data[] = Object.values(Spacing)
+  .sort(function(a, b) {
+    return b - a;
+  })
+  .map((value: SpacingScaleValues) => {
+    return {
+      value: value,
+      usage: value > 64 ? 'Layout' : 'Element Layout',
+    };
+  });
+
+const columns = [
+  { title: 'Value', dataIndex: 'value' },
+  {
+    title: 'Example',
+    dataIndex: 'example',
+    // eslint-disable-next-line react/display-name
+    render: (data: Data) => (
+      <div
+        style={{
+          width: `${data.value}px`,
+          height: `${data.value}px`,
+          background: SecondaryColor.violet,
+        }}
+      />
+    ),
+  },
+  {
+    title: 'Recommended usage',
+    dataIndex: 'usage',
+  },
+];
+
+const SpacingStory = () => (
+  <StorybookComponent title="Spacing" usage="...">
+    <StorybookTable dataSource={dataSource} columns={columns} />
+  </StorybookComponent>
+);
+
+export default SpacingStory;
---
+++ stories/StorybookTable.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { Greyscale } from '../src/Utils/Colors';
+
+interface Data {
+  [key: string]: any;
+}
+
+interface Column {
+  title: string;
+  dataIndex: string;
+  render?: (data: Data) => React.ReactNode;
+}
+
+const Table = styled.table`
+  border-collapse: collapse;
+`;
+
+const Tr = styled.tr`
+  border-bottom: 1px solid ${Greyscale.lightgrey};
+`;
+
+const Th = styled.th`
+  padding: 10px 20px;
+  font-weight: normal;
+  background-color: ${Greyscale.softgrey};
+  text-align: left;
+`;
+
+const Td = styled.td`
+  padding: 10px 20px;
+  vertical-align: top;
+`;
+
+export const StorybookTable: React.FC<{
+  dataSource: Data[];
+  columns: Column[];
+}> = ({ dataSource, columns }) => {
+  const columnTitles = columns.map(column => column.title);
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          {columnTitles.map(title => (
+            <Th key={title}>{title}</Th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {dataSource.map((data, index) => (
+          <Tr key={index}>
+            {columns.map(column => {
+              return (
+                <Td key={column.dataIndex}>
+                  {column.render ? column.render(data) : data[column.dataIndex]}
+                </Td>
+              );
+            })}
+          </Tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
