2b280d67cd436319d3de23dc0a7df15e80a7b680
ross <ross@glints.com>
2020-09-21 19:06:33 +0800

    Box: add Box component with space props

---
+++ stories/Layout/BoxStory/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import BoxSpaceStory from './BoxSpaceStory';
+import Divider from '../../../src/General/Divider';
+
+const BoxStories = () => (
+  <>
+    <BoxSpaceStory />
+    <Divider theme="grey" />
+  </>
+);
+
+export default BoxStories;
