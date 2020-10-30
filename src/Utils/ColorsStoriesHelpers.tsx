46fb1d8a0ca514cf0b88149343697e2d4ea54f71
marco <marco@glints.com>
2020-10-30 09:30:40 +0800

    Documentation: Upgrade color story

---
+++ src/Utils/ColorsStoriesHelpers.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ColorSampler = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+
+export interface ColorBoxProps {
+  bgColor: string;
+  fontColor: string;
+}
+
+const StyledColorSample = styled.div`
+  margin: 1em;
+  flex: '1 20%';
+`;
+
+const ColorBox = styled.div<ColorBoxProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 120px;
+  height: 80px;
+  margin: 1em auto;
+  background-color: ${({ bgColor }) => bgColor};
+  font-size: 14px;
+  font-weight: bold;
+  color: ${({ fontColor }) => fontColor};
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.15);
+`;
+
+const ColorLabel = styled.code`
+  display: flex;
+  justify-content: center;
+  font-size: 16px;
+`;
+
+export interface ColorSampleProps extends ColorBoxProps {
+  name: string;
+}
+
+export const ColorSample: React.FunctionComponent<ColorSampleProps> = ({
+  bgColor,
+  fontColor,
+  name,
+}) => (
+  <StyledColorSample>
+    <ColorBox bgColor={bgColor} fontColor={fontColor}>
+      <code>{bgColor}</code>
+    </ColorBox>
+    <ColorLabel>{name}</ColorLabel>
+  </StyledColorSample>
+);
