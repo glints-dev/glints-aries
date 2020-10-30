59c3b8df146419810f7f0c1b44204d8e53569c5d
marco <marco@glints.com>
2020-10-30 13:33:46 +0800

    GlintsContainer: Extract BaseStyles to be used individually

--- src/Input/Checkbox/Checkbox.tsx
+++ src/Input/Checkbox/Checkbox.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 import { CheckboxContainer } from './CheckboxStyle';
 import { isUndefined } from 'lodash';
 
-const Checkbox: React.FunctionComponent<CheckboxProps> = ({
+export const Checkbox: React.FunctionComponent<CheckboxProps> = ({
   id,
   label,
   value,
