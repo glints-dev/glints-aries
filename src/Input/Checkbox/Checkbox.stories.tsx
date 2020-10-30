5b938fcbcfb04cbfb5cd9492894b3f82c937cb47
marco <marco@glints.com>
2020-10-30 13:34:10 +0800

    Documentation: Migrate checkbox story

--- src/Input/Checkbox/Checkbox.stories.tsx
+++ src/Input/Checkbox/Checkbox.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Story, Meta } from '@storybook/react';
 
 import { Checkbox, CheckboxProps } from './Checkbox';
@@ -7,17 +7,30 @@ import { BaseContainer } from '../../Layout/GlintsContainer/GlintsContainer';
 export default {
   title: 'General/Checkbox',
   component: Checkbox,
+  decorators: [Story => <BaseContainer>{Story()}</BaseContainer>],
 } as Meta;
 
-const Template: Story<CheckboxProps> = args => (
-  <BaseContainer>
-    <Checkbox {...args} />
-  </BaseContainer>
-);
+const Template: Story<CheckboxProps> = args => <Checkbox {...args} />;
 
 export const Interactive = Template.bind({});
 Interactive.args = {
   id: 'software-engineer',
   value: 'software-engineer',
   label: 'Software Engineer',
+};
+
+export const Bordered = Template.bind({});
+Bordered.args = {
+  id: 'bordered-engineer',
+  value: 'bordered-engineer',
+  label: 'Bordered Engineer',
+  border: true,
+};
+
+export const Sizes = Template.bind({});
+Sizes.args = {
+  id: 'large-engineer',
+  value: 'large-engineer',
+  label: 'Large Engineer',
+  size: 'large',
 };
