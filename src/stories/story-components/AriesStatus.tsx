3d874944488e68d74aef5c4670a8f3f946bdec79
marco <marco@glints.com>
2020-11-09 14:42:20 +0800

    Add status table to Introduction story

---
+++ src/stories/story-components/AriesStatus.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const QUERY = 'SELECT * WHERE H<>"Removing" ORDER BY F DESC';
+
+export const AriesStatus = () => {
+  const [data, setData] = useState('');
+  useEffect(function fetchStatusFromGoogleSheets() {
+    fetch(
+      `https://docs.google.com/spreadsheets/u/0/d/1O9WY_x9MmzRIbskPE-2_TsKgOz3N7C61iEqcx6zWln8/gviz/tq?tqx=out:html&tq=${encodeURIComponent(
+        QUERY
+      )}`
+    )
+      .then(res => res.text())
+      .then(
+        result => {
+          setData(result);
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }, []);
+  return <div dangerouslySetInnerHTML={{ __html: data }}></div>;
+};
