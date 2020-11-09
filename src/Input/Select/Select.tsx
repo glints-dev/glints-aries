bcf1b5e9a16be960424e8a5c3da9ffdc2fa1df43
ross <ross@glints.com>
2020-11-09 11:44:05 +0800

    Select: mirgate to storybook 6

--- src/Input/Select/Select.tsx
+++ src/Input/Select/Select.tsx
@@ -17,7 +17,12 @@ import {
 
 import { canWarn } from '../../Utils/canWarn';
 
-const Select: ISelect = (props: Props) => {
+const defaultFilterFunction = (option: string, search: string) =>
+  toLower(option).includes(toLower(search));
+
+const Select: React.FC<Props> & {
+  Option: React.FC<SelectItemProps>;
+} = props => {
   const {
     label,
     status,
@@ -39,7 +44,7 @@ const Select: ISelect = (props: Props) => {
     defaultOpen = false,
     children,
     isLoading,
-    filterFunction,
+    filterFunction = defaultFilterFunction,
     ...defaultProps
   } = props;
 
@@ -116,10 +121,8 @@ const Select: ISelect = (props: Props) => {
       }
     }
 
-    const matchedChildrenOptions = childrenOptions.filter(
-      filterFunction
-        ? data => filterFunction(data.props.children, inputValue)
-        : data => toLower(data.props.children).includes(toLower(inputValue))
+    const matchedChildrenOptions = childrenOptions.filter(data =>
+      filterFunction(data.props.children, inputValue)
     );
     return matchedChildrenOptions;
   }, [children, inputValue, disableTyping, isInputChange, filterFunction]);
@@ -401,32 +404,54 @@ const Select: ISelect = (props: Props) => {
   );
 };
 
-export interface Props
-  extends React.ComponentPropsWithoutRef<typeof SelectInput> {
-  children: React.ReactNode;
-  isLoading?: boolean;
-  label?: string;
-  noOptionResult?: string;
-  removeDropIcon?: boolean;
-  removeFloatingLabel?: boolean;
-  error?: React.ReactNode | string | boolean;
-  renderError?: (error: React.ReactNode | string | boolean) => React.ReactNode;
-  value?: string;
-  defaultValue?: string;
-  defaultOpen?: boolean;
-  filterFunction?: (option: string, search: string) => boolean;
-
-  onFocus?(e: React.FocusEvent<HTMLInputElement>): void;
-  onBlur?(e: React.FocusEvent<HTMLInputElement>): void;
-  onChange?(value: string): void;
-  onInputChange?(e: React.ChangeEvent<HTMLInputElement>): void;
+export interface SelectInputProps {
+  /** Disable typing on Select input, so the option can't be searched. */
+  disableTyping?: boolean;
+  /** Sets Select to be smaller. */
+  small?: boolean;
+  /** Deprecated! */
+  status?: string;
 }
 
-type ISelect = React.FunctionComponent<Props> & {
-  Option: React.FunctionComponent<SelectItemProps>;
-};
+export interface Props
+  extends React.ComponentPropsWithoutRef<typeof SelectInput>,
+    SelectInputProps {
+  children: React.ReactNode;
+  /** Sets value for Input. */
+  value?: string;
+  /** Sets default value for Input. */
+  defaultValue?: string;
+  /** Disable the Text Field. */
+  disabled?: boolean;
+  /** To disable browser auto complete, pass "nope" or anything other than "off". */
+  autoComplete?: string;
+  /** Shows loading spinner in menu. */
+  isLoading?: boolean;
+  /** Sets placeholder value for the Text Field. */
+  label?: string;
+  /** Sets message to display when no option matches. */
+  noOptionResult?: string;
+  /** Removes drop icon. */
+  removeDropIcon?: boolean;
+  /** Removes floating effect for placeholder. */
+  removeFloatingLabel?: boolean;
+  /** Pass true to display error state without error message */
+  error?: React.ReactNode | string | boolean;
+  /** Deprecated! */
+  renderError?: (error: React.ReactNode | string | boolean) => React.ReactNode;
+  /** Sets initial open state of Select. */
+  defaultOpen?: boolean;
+  /** A custom function to filter options by when typing into Select. By default, it filters options by input with case insensitive. However, if the input value is case sensitive same as one of options, then filterFunction will be ignored.  */
+  filterFunction?: (option: string, search: string) => boolean;
+  onFocus?(e: React.FocusEvent<HTMLInputElement>): void;
+  onBlur?(e: React.FocusEvent<HTMLInputElement>): void;
+  /** Called when select an option or value of input is changed. WARNING: firing on value of input is changed will be deprecated, please use onInputChange instead */
+  onChange?(value: string): void;
+  /** Called when value of input is changed. */
+  onInputChange?(event: React.ChangeEvent<HTMLInputElement>): void;
+}
 
-export const Option: React.FunctionComponent<SelectItemProps> = () => null;
+export const Option: React.FC<SelectItemProps> = () => null;
 Option.displayName = 'Select.Option';
 
 Select.Option = Option;
