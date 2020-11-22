9d845fa0d866fbb50a176f7aeb7277ce03dc5528
ritikamotwani <ritika.motwani0712@gmail.com>
2020-11-23 00:33:41 +0530

    Documentation: Add text area story according to the updates version

---
+++ src/Input/Textarea/Textarea.stories.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import { Props, Textarea } from './Textarea';
+import { BaseContainer } from '../../Layout/GlintsContainer/GlintsContainer';
+
+export default {
+  title: 'Input/Textarea',
+  component: Textarea,
+  decorators: [Story => <BaseContainer>{Story()}</BaseContainer>],
+} as Meta;
+
+const Template: Story<Props> = args => <Textarea {...args} />;
+
+export const Interactive = Template.bind({});
+Interactive.args = {
+  label: 'Description',
+};
+
+export const removeFloatingLabelIsTrue = Template.bind({});
+removeFloatingLabelIsTrue.args = {
+  label: 'Remove Floating Label Is True',
+  removeFloatingLabel: true,
+};
