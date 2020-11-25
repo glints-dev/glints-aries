6289ad053d18e17b63f617ebcb1cc5238bbdfc3b
marco <marco@glints.com>
2020-11-25 11:49:57 +0800

    Heading: Migrate story

--- src/General/Heading/HeadingStyle.ts
+++ src/General/Heading/HeadingStyle.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { PrimaryColor } from '../../Utils/Colors';
+import { Props } from './Heading';
 
-export const HeadingContainer = styled.h1<HeadingContainerProps>`
+export const HeadingContainer = styled.h1<Props>`
   position: relative;
   display: ${({ inline }) => (inline ? 'inline-block' : 'block')};
   font-size: 2em;
@@ -12,8 +13,3 @@ export const HeadingContainer = styled.h1<HeadingContainerProps>`
     background-image: ${`linear-gradient(to top, ${PrimaryColor.glintsyellow} 40%, transparent 0)`};
   }
 `;
-
-interface HeadingContainerProps {
-  inline?: boolean;
-  uppercaseText?: boolean;
-}
