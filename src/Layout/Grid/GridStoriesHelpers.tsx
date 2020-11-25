170c7caf619bc8462b0be5d83002dd900eb30a1c
marco <marco@glints.com>
2020-11-25 18:56:34 +0800

    Grid: Migrate stories

---
+++ src/Layout/Grid/GridStoriesHelpers.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Greyscale } from '../../Utils/Colors';
+
+export const Cell = styled.div`
+  background-color: ${Greyscale.softgrey};
+  text-align: center;
+  padding: 8px;
+  border: 1px solid black;
+`;
