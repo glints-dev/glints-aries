e53463523860e24fa9a3924667dc7d06005009d5
marco <marco@glints.com>
2020-11-25 18:56:34 +0800

    Grid: Migrate stories

--- src/Layout/Grid/Row.tsx
+++ src/Layout/Grid/Row.tsx
@@ -18,7 +18,9 @@ const Row: React.FunctionComponent<Props> = ({
 
 interface Props {
   children: React.ReactNode;
+  /** Sets the align-content and align-items css properties for the row. */
   alignContent?: string;
+  /** Sets the justfiy-content and justfiy-items css properties for the row. */
   justifyContent?: string;
 }
 
