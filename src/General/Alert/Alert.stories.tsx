96fb83e9f9974955bb7038e0cb7c1c73243fe5ae
kevin gondokusumo <kevingondokusumo@kevins-MacBook-Pro.local>
2020-12-02 22:56:00 +0800

    Alert: make alert stories for storybook 6

---
+++ src/General/Alert/Alert.stories.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import { Alert, Props } from './Alert';
+import { Button } from '../Button';
+import { BaseContainer } from '../../Layout/GlintsContainer/GlintsContainer';
+
+export default {
+  title: 'General/Alert',
+  component: Alert,
+  decorators: [Story => <BaseContainer>{Story()}</BaseContainer>],
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+      },
+    },
+  },
+} as Meta;
+
+const Template: Story<Props> = args => {
+  const [isOpen, setOpen] = useState(args.isOpen);
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)} variant="solid-blue">
+        Show Pop up
+      </Button>
+      <Alert {...args} isOpen={isOpen} onClose={() => setOpen(false)} />
+    </>
+  );
+};
+
+export const Interactive = Template.bind({});
+Interactive.args = {
+  type: 'info',
+  message: 'You have successfully applied to Glints as Software Engineer.',
+  autoClose: 3000,
+};
