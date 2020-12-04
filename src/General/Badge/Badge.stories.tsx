fd88757d22856cd07aeadfc30500e048cddd176e
kevin gondokusumo <kevingondokusumo@kevins-MacBook-Pro.local>
2020-12-05 00:20:09 +0800

    Badge: make badge stories

--- src/General/Badge/Badge.stories.tsx
+++ src/General/Badge/Badge.stories.tsx
@@ -3,25 +3,37 @@ import { Story, Meta } from '@storybook/react';
 
 import { Badge, Props } from './Badge';
 import { BaseContainer } from '../../Layout/GlintsContainer/GlintsContainer';
+import styled from 'styled-components';
+
+const BadgeStack = styled('div')`
+  display: flex;
+  align-items: flex-start;
+`;
 
 export default {
   title: 'General/Badge',
   component: Badge,
   decorators: [Story => <BaseContainer>{Story()}</BaseContainer>],
-  argTypes: {
-    variant: {
-      control: {
-        type: 'radio',
-        options: ['dimmed', 'default'],
-      },
-    },
-  },
 } as Meta;
 
 const Template: Story<Props> = args => <Badge {...args}></Badge>;
 
-export const SolidBlue = Template.bind({});
+export const Interactive = Template.bind({});
 
-SolidBlue.args = {
+Interactive.args = {
   label: 'hello',
 };
+
+export const Variants: Story<Props> = () => (
+  <BadgeStack>
+    <Badge label="default" variant="default" />
+    <Badge label="dimmed" variant="dimmed" />
+  </BadgeStack>
+);
+
+export const Sup: Story<Props> = () => (
+  <BadgeStack>
+    <Badge label="not superscript" sup={false} />
+    <Badge label="superscript" sup={true} />
+  </BadgeStack>
+);
