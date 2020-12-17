8178bbbd5e6ded7b7251f45788520f4f0df7886d
marco <marco@glints.com>
2020-12-17 18:34:24 +0800

    Tabs: Migrate to storybook 6

---
+++ src/Display/Tabs/Tabs.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import { Tabs, Props } from './Tabs';
+import { BaseContainer } from '../../Layout/GlintsContainer/GlintsContainer';
+
+export default {
+  title: 'Display/Tabs',
+  component: Tabs,
+  decorators: [Story => <BaseContainer>{Story()}</BaseContainer>],
+  argTypes: {
+    className: {
+      table: {
+        disable: true,
+      },
+    },
+    activeTab: {
+      control: {
+        type: null,
+      },
+    },
+    onTabClick: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta;
+
+const Template: Story<Props> = ({ ...args }) => (
+  <Tabs {...args}>
+    <Tabs.Pane tab="Location Tab">Tab Location</Tabs.Pane>
+    <Tabs.Pane tab="Salary">Tab Salary</Tabs.Pane>
+    <Tabs.Pane tab="Job">Tab Job</Tabs.Pane>
+    <Tabs.Pane tab="Company">Tab Company</Tabs.Pane>
+  </Tabs>
+);
+
+export const Interactive = Template.bind({});
+
+export const Variant = Template.bind({});
+Variant.args = {
+  variant: 'colored',
+};
+
+export const Alignment = Template.bind({});
+Alignment.args = {
+  alignment: 'vertical',
+};
+
+export const Theme = Template.bind({});
+Theme.args = {
+  theme: 'blue',
+};
