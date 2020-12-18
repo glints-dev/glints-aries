b0d7f9ce65d2eaf5a9b4fac762776440d6f784f9
ross <ross@glints.com>
2020-12-18 18:33:25 +0800

    Shadows: add Shadows and export it

---
+++ src/Utils/Shadows.ts
@@ -0,0 +1,15 @@
+export const Shadows = {
+  1: {
+    up: '0 2px 4px 0 rgba(0, 0, 0, 0.15)',
+  },
+  2: {
+    up: '0 4px 8px 0 rgba(0, 0, 0, 0.15)',
+  },
+  3: {
+    up: '0 6px 12px 0 rgba(0, 0, 0, 0.10)',
+  },
+  4: {
+    up: '0 12px 24px 0 rgba(0, 0, 0, 0.10)',
+    down: '0 -6px 12px 0 rgba(0, 0, 0, 0.10)',
+  },
+};
