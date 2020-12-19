7875a68eb467275851593ef5ec2b33ac58668b81
ross <ross@glints.com>
2020-12-19 14:19:06 +0800

    Shadows: Box supports boxShadow prop

--- src/Utils/Shadows.ts
+++ src/Utils/Shadows.ts
@@ -1,15 +1,28 @@
+const ShadowOneDown = '0 2px 4px 0 rgba(0, 0, 0, 0.15)';
+const ShadowTwoDown = '0 4px 8px 0 rgba(0, 0, 0, 0.15)';
+const ShadowThreeDown = '0 6px 12px 0 rgba(0, 0, 0, 0.10)';
+const ShadowFiveDown = '0 12px 24px 0 rgba(0, 0, 0, 0.10)';
+const ShadowFiveUp = '0 -6px 12px 0 rgba(0, 0, 0, 0.10)';
+
 export const Shadows = {
   1: {
-    up: '0 2px 4px 0 rgba(0, 0, 0, 0.15)',
+    down: ShadowOneDown,
   },
   2: {
-    up: '0 4px 8px 0 rgba(0, 0, 0, 0.15)',
+    down: ShadowTwoDown,
   },
   3: {
-    up: '0 6px 12px 0 rgba(0, 0, 0, 0.10)',
+    down: ShadowThreeDown,
   },
   4: {
-    up: '0 12px 24px 0 rgba(0, 0, 0, 0.10)',
-    down: '0 -6px 12px 0 rgba(0, 0, 0, 0.10)',
+    down: ShadowFiveDown,
+    up: ShadowFiveUp,
   },
-};
+} as const;
+
+export type ShadowValueType =
+  | typeof ShadowOneDown
+  | typeof ShadowTwoDown
+  | typeof ShadowThreeDown
+  | typeof ShadowFiveDown
+  | typeof ShadowFiveUp;
