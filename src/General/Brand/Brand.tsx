3d0bc9992d70589e7e807ba80c14719443723a32
ross <ross@glints.com>
2020-12-21 15:39:53 +0800

    Brand: migrate Brand to storybook v6

--- src/General/Brand/Brand.tsx
+++ src/General/Brand/Brand.tsx
@@ -57,11 +57,16 @@ const Brand = ({
   );
 };
 
-interface Props extends React.ComponentPropsWithoutRef<typeof BrandContainer> {
+export interface Props
+  extends React.ComponentPropsWithoutRef<typeof BrandContainer> {
+  /** There are 3 available values for asset props. The first and second one are Glints logo, but you can also custom the logo by parsing url or path of the logo. */
   asset: string;
+  /** HTML <img> alt Attribute */
   alt?: HTMLImageElement['alt'];
   className?: string;
+  /** Redirect to url when right click is triggered. */
   rightClickURL?: string;
+  /** Executes when the user right-clicks on the component */
   onContextMenu?(): void;
 }
 
