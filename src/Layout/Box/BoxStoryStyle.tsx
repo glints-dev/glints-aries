2606f875451cab4aac41b32b5abac24aea3cb02c
ross <ross@glints.com>
2021-01-15 14:09:04 +0800

    Box: migrate Box storybook to v6

---
+++ src/Layout/Box/BoxStoryStyle.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import { Box as BaseBox } from '../../../src/Layout/Box';
+
+export const GreyBackground = styled.div`
+  display: inline-block;
+  background-color: #f3f3f3;
+`;
+
+export const Content = styled.span`
+  background-color: white;
+`;
+
+export const Box = styled(BaseBox)`
+  background-color: #4caf50;
+`;
