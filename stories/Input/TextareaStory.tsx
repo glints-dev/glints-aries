5155b47e4468fcf68b1046b91953286ddd05d4da
ritikamotwani <ritika.motwani0712@gmail.com>
2021-01-18 00:38:33 +0530

    TextArea: Add error and small text areas in the story

--- stories/Input/TextareaStory.tsx
+++ stories/Input/TextareaStory.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 import StorybookComponent from '../StorybookComponent';
 
 import Textarea from '../../src/Input/Textarea';
+import Divider from '../../src/General/Divider';
+import Heading from '../../src/General/Heading';
 
 const props = {
   Textarea: [
@@ -46,20 +48,74 @@ const props = {
       require: 'no',
       description: 'Removes floating effect for placeholder.',
     },
+    {
+      name: 'small',
+      type: 'boolean',
+      defaultValue: <code>false</code>,
+      possibleValue: <code>true | false</code>,
+      require: 'no',
+      description: 'Sets the size of the text area.',
+    },
   ],
 };
 
 const TextareaStory = () => (
-  <StorybookComponent
-    title="Textarea"
-    code="import { Textarea } from 'glints-aries'"
-    propsObject={props}
-    usage={'<Textarea label="Description" />'}
-  >
-    <div style={{ width: '300px' }}>
-      <Textarea label="Description" />
-    </div>
-  </StorybookComponent>
+  <>
+    <StorybookComponent
+      title="Textarea"
+      code="import { Textarea } from 'glints-aries'"
+      propsObject={props}
+      usage={'<Textarea label="Description" removeFloatingLabel={true} />'}
+    >
+      <div style={{ width: '300px' }}>
+        <Textarea label="Description" removeFloatingLabel={true} />
+      </div>
+    </StorybookComponent>
+    <Divider theme="grey" />
+    <StorybookComponent
+      propsObject={props}
+      usage={
+        '<Textarea label="Description" status="error" removeFloatingLabel={true} />'
+      }
+    >
+      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
+        Text Area In Error State
+      </Heading>
+      <div style={{ width: '300px' }}>
+        <Textarea
+          label="Description"
+          status="error"
+          removeFloatingLabel={true}
+        />
+      </div>
+    </StorybookComponent>
+    <Divider theme="grey" />
+    <StorybookComponent
+      propsObject={props}
+      usage={'<Textarea label="Description"  />'}
+    >
+      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
+        Text Area With Floating Label
+      </Heading>
+      <div style={{ width: '300px' }}>
+        <Textarea label="Description" />
+      </div>
+    </StorybookComponent>
+    <Divider theme="grey" />
+    <StorybookComponent
+      propsObject={props}
+      usage={
+        '<Textarea label="Description" small={true} removeFloatingLabel={true}  />'
+      }
+    >
+      <Heading style={{ fontSize: '20px', marginBottom: '1em' }}>
+        Small Text Area
+      </Heading>
+      <div style={{ width: '300px' }}>
+        <Textarea label="Description" small={true} removeFloatingLabel={true} />
+      </div>
+    </StorybookComponent>
+  </>
 );
 
 export default TextareaStory;
