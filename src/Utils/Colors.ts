540c815b01d93ae191c3db246c0507cbc6894e15
marco <marco@glints.com>
2021-01-26 11:34:26 +0800

    Colors: Remove deprecated colors

--- src/Utils/Colors.ts
+++ src/Utils/Colors.ts
@@ -6,41 +6,18 @@ export const PrimaryColor = {
 
 export const SecondaryColor = {
   actionblue: '#017EB7',
+  darkblue: '#132647',
   green: '#93BD49',
   darkgreen: '#008464',
   magenta: '#FF00E5',
   violet: '#9013FE',
   orange: '#F5A623',
-
-  // deprecated
-  ivory: '#FFFAEA',
-  lighterblack: '#AAAAAA',
-  lightergrey: '#EEEEEE',
-  blue: '#0869C2',
-  darkerblue: '#027EB7',
-  lightgreen: '#EEF6F4',
-  lightorange: '#FFF6E8',
-  lightred: '#FFF2F3',
-  lightblue: '#D6F4FF',
-  darkblue: '#004b68',
-
-  // renamed
-  purplepizza: '#FF00E5',
-  bluegem: '#9013FE',
-
-  // moved to Greyscale
-  white: '#FFFFFF',
-  whitesmoke: '#F3F3F3',
-  black: '#000000',
-  lightblack: '#777777',
-  grey: '#777777',
-  darkgrey: '#777777',
-  lightgrey: '#C6C6C6',
 };
 
 export const Greyscale = {
   black: '#000000',
   grey: '#777777',
+  devilsgrey: '#666666',
   lightgrey: '#C6C6C6',
   softgrey: '#F3F3F3',
   white: '#FFFFFF',
