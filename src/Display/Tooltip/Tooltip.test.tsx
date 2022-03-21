98001187a4430ffe78bedc04c1973127b9a54fbf
Yong Jie Wong <yjwong92@gmail.com>
2022-03-21 23:39:01 +0800

    Tooltip: Remove test relying on internal implementation details.
    The hideTooltipIfTouchOutside callback is really an internal
    implementation detail, and the functionality would have been covered by
    the test cases right above it.

--- src/Display/Tooltip/Tooltip.test.tsx
+++ src/Display/Tooltip/Tooltip.test.tsx
@@ -85,28 +85,6 @@ describe('<Tooltip/> touch event', () => {
     fireEvent.touchStart(outside);
     expect(queryByText('mock text')).not.toBeInTheDocument();
   });
-
-  test('touch outside should register and unregister hideTooltipIfTouchOutside callback', () => {
-    const addEventListenerSpy = jest.spyOn(document, 'addEventListener');
-    const removeEventListenerSpy = jest.spyOn(document, 'removeEventListener');
-    const useCallbackSpy = jest.spyOn(React, 'useCallback');
-
-    const { queryByText } = render(<TooltipComponent />);
-    const element = queryByText('mock content');
-    fireEvent.touchStart(element);
-    expect(addEventListenerSpy.mock.calls[0][1]).toEqual(
-      useCallbackSpy.mock.results[0].value
-    );
-
-    const outside = queryByText('touch outside');
-    fireEvent.touchStart(outside);
-
-    expect(removeEventListenerSpy.mock.calls[0][1]).toEqual(
-      useCallbackSpy.mock.results[0].value
-    );
-
-    jest.clearAllMocks();
-  });
 });
 
 describe('<Tooltip/> snapshot with html content', () => {
