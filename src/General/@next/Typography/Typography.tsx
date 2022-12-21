b9ae6d39c152d620bf305e4adc809e7a3319b158
Nina Benite <nina.benite@glints.com>
2022-12-21 14:44:28 +0800

    Typography: warn if variant is not of type Variant

--- src/General/@next/Typography/Typography.tsx
+++ src/General/@next/Typography/Typography.tsx
@@ -2,20 +2,23 @@ import React, { ElementType, FunctionComponent, ReactNode } from 'react';
 
 import { StyledTypography } from './TypographyStyles';
 
-export type Variant =
-  | 'headline1'
-  | 'headline2'
-  | 'headline3'
-  | 'headline4'
-  | 'headline5'
-  | 'headline6'
-  | 'subtitle1'
-  | 'subtitle2'
-  | 'body1'
-  | 'body2'
-  | 'button'
-  | 'caption'
-  | 'overline';
+const typographyVariant = [
+  'headline1',
+  'headline2',
+  'headline3',
+  'headline4',
+  'headline5',
+  'headline6',
+  'subtitle1',
+  'subtitle2',
+  'body1',
+  'body2',
+  'button',
+  'caption',
+  'overline',
+] as const;
+
+export type Variant = (typeof typographyVariant)[number];
 
 export interface TypographyProps {
   /** Sets the Typography's element */
@@ -27,12 +30,20 @@ export interface TypographyProps {
   variant?: Variant;
 }
 
+const isVariant = (str: any): str is Variant => typographyVariant.includes(str);
+
 export const Typography: FunctionComponent<TypographyProps> = ({
   as = 'p',
   children,
   variant,
   ...props
 }) => {
+  if (!isVariant(variant)) {
+    console.warn(
+      `Variant: ${variant} is not of type Variant | undefined. \ntype Variant ${typographyVariant}`
+    );
+  }
+
   return (
     <StyledTypography as={as} variant={variant} {...props}>
       {children}
