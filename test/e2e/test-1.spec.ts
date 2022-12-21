40f4470edb9a8dc7c7042d1c371591e7e3f21f95
Marthayaputra <marthayaputra@glints.com>
2022-12-21 15:08:38 +0800

    feat: add visual test job

---
+++ test/e2e/test-1.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:6006/?path=/docs/utilities-colors--page');
+  const yellow = await page
+    .frameLocator('internal:attr=[title="storybook-preview-iframe"i]')
+    .locator(
+      'div:nth-child(12) > div > .ColorsStoriesHelpers__ColorBox-sc-1euu88z-2'
+    )
+    .first();
+
+  await expect(yellow).toHaveScreenshot('yellow.png');
+});
