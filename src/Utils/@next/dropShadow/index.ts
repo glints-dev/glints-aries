2769892831ea6ea155781a1c37f3185ca62372ba
Marthayaputra <marthayaputra@glints.com>
2022-12-27 09:19:49 +0800

    feat: add button focus drop shadow

--- src/Utils/@next/dropShadow/index.ts
+++ src/Utils/@next/dropShadow/index.ts
@@ -5,6 +5,7 @@ export const base = `0 0 0 1px rgba(63, 63, 68, 0.05),
 export const deep = `0 0 0 1px rgba(6, 44, 82, 0.1),
   0 2px 16px rgba(33, 43, 54, 0.08)`;
 export const button = '0 1px 0 rgba(0, 0, 0, 0.05)';
+export const buttonFocused = '0px 0px 0px 2px rgba(106, 201, 236, 1)';
 export const topBar = '0 2px 2px -1px rgba(0, 0, 0, 0.15)';
 export const card = `0 0 5px rgba(23, 24, 24, 0.05), 
   0 1px 2px rgba(0, 0, 0, 0.15)`;
