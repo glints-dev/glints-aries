bc50d869bd595ab5e53f5c07c7b6eb596d375838
Marthayaputra <marthayaputra@glints.com>
2023-01-31 16:39:45 +0700

    test: add canvas on storybook page

--- test/e2e/divider/divider.spec.ts
+++ test/e2e/divider/divider.spec.ts
@@ -1,11 +1,11 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { StoryBookPage } from '../storybookPage';
 
-const getPage = (page: Page) =>
-  new StoryBookPage(page, '?path=/story/next-divider--interactive');
-
-test.skip('Divider', async ({ page }) => {
-  const dividerPage = getPage(page);
+test('Divider', async ({ page }) => {
+  const dividerPage = new StoryBookPage(
+    page,
+    '?path=/story/next-divider--interactive'
+  );
   await dividerPage.goto();
   await expect(dividerPage.container).toHaveScreenshot('divider.png');
 });
