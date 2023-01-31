e948fea74652b5ae0e923269859c7cb07b6f9a66
Marthayaputra <marthayaputra@glints.com>
2023-01-31 17:39:58 +0700

    test: enable divider

--- test/e2e/divider/divider.spec.ts
+++ test/e2e/divider/divider.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { StoryBookPage } from '../storybookPage';
 
-test.skip('Divider', async ({ page }) => {
+test('Divider', async ({ page }) => {
   const dividerPage = new StoryBookPage(
     page,
     '?path=/story/next-divider--interactive'
