f52ed4f87b03a810da76a00447ac3a59ae122857
marthayaputra <105256293+marthayaputra@users.noreply.github.com>
2023-02-01 14:55:50 +0700

    Feature/missing dts files

--- src/helpers/storybook/Decorators.tsx
+++ src/helpers/storybook/Decorators.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { DecoratorFn } from '@storybook/react';
-import { AlertProvider } from '../src/@next/Alert/AlertProvider';
+import { AlertProvider } from '../../@next/Alert/AlertProvider';
 
 // Receive a component and apply a bordered container with max width of 400px.
 export const withAlertProvider: DecoratorFn = Story => (
