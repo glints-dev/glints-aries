91b69059ea0836f352e60e855ae1984d8d2fba98
Marthayaputra <marthayaputra@glints.com>
2023-02-16 11:49:44 +0800

    fix: flaky indextable tests

--- test/e2e/indexTable/indexTable.spec.ts
+++ test/e2e/indexTable/indexTable.spec.ts
@@ -5,8 +5,6 @@ test('IndexTable', async ({ page }) => {
   const indexTablePage = new IndexTablePage(page);
   await indexTablePage.goto();
 
-  await page.waitForLoadState('domcontentloaded');
-
   await expect(indexTablePage.canvas).toHaveScreenshot('indexTable.png');
 
   await indexTablePage.tableRow.hover();
@@ -19,14 +17,14 @@ test('IndexTable - bulk action', async ({ page }) => {
   const indexTablePage = new IndexTablePage(page);
   await indexTablePage.goto();
 
-  await page.waitForLoadState('domcontentloaded');
-
   await indexTablePage.checkboxes.first().click();
+  await page.waitForTimeout(300);
   await expect(indexTablePage.canvas).toHaveScreenshot(
     'indextable-selectall-rows.png'
   );
 
   await indexTablePage.checkboxes.last().click();
+  await page.waitForTimeout(300);
   await expect(indexTablePage.canvas).toHaveScreenshot(
     'indextable-selectsome-rows.png'
   );
@@ -41,8 +39,6 @@ test('IndexTable - loading state', async ({ page }) => {
   const indexTablePage = new IndexTablePage(page);
   await indexTablePage.goto('args=loading:true');
 
-  await page.waitForLoadState('domcontentloaded');
-
   await expect(indexTablePage.canvas).toHaveScreenshot(
     'indexTable-loading.png'
   );
@@ -52,8 +48,6 @@ test('IndexTable - empty state', async ({ page }) => {
   const indexTablePage = new IndexTablePage(page);
 
   await indexTablePage.goto('args=itemCount:0');
-
-  await page.waitForLoadState('domcontentloaded');
 
   await expect(indexTablePage.canvas).toHaveScreenshot(
     'indexTable-empty-state.png'
