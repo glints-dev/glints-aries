40484264b53473d56ffb0179c2c199fc1b76d9c6
Marthayaputra <marthayaputra@glints.com>
2023-02-17 15:24:02 +0800

    chore: remove typography

--- src/@next/Card/Section.tsx
+++ src/@next/Card/Section.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
-import { Typography } from '../Typography';
 import { StyledCardSection } from './CardStyle';
 
 export const Section = ({ children }: { children: React.ReactNode }) => {
-  const content =
-    typeof children === 'string' ? (
-      <Typography as="div" variant="body1">
-        {children}
-      </Typography>
-    ) : (
-      children
-    );
-  return <StyledCardSection className="section">{content}</StyledCardSection>;
+  return <StyledCardSection className="section">{children}</StyledCardSection>;
 };
