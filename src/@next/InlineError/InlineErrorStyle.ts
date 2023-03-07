4918f3e643ca33d6ba3ba50841785d44406a6fb9
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2023-03-07 09:56:26 +0700

    feat: improve inline error icon position

--- src/@next/InlineError/InlineErrorStyle.ts
+++ src/@next/InlineError/InlineErrorStyle.ts
@@ -2,11 +2,23 @@ import styled from 'styled-components';
 import { space4 } from '../utilities/spacing';
 import { Typography } from '../Typography';
 import { Red } from '../utilities/colors';
+import { Breakpoints } from '..';
 
 export const StyledWrapper = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   gap: ${space4};
+
+  svg {
+    flex-shrink: 0;
+  }
+
+  @media (max-width: ${Breakpoints.large}) {
+    svg {
+      height: 18px;
+      width: 18px;
+    }
+  }
 `;
 
 export const StyledText = styled(Typography)`
