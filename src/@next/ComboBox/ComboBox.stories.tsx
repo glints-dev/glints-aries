a1fccf5fb31bf31321ac9dbbc40bd8cd8c5ebd42
Nina Benite <nina.benite@glints.com>
2023-03-08 18:58:34 +0800

    ComboBox story source code

--- src/@next/ComboBox/ComboBox.stories.tsx
+++ src/@next/ComboBox/ComboBox.stories.tsx
@@ -62,7 +62,7 @@ const MultiSelectTemplate: Story<ComboBoxProps> = args => {
   };
 
   const optionsMarkup =
-    options.length > 0
+    options?.length > 0
       ? options.map(option => {
           const { label, value } = option;
 
@@ -132,4 +132,115 @@ export const MultiSelect = MultiSelectTemplate.bind({});
 
 MultiSelect.args = {
   allowMultiple: true,
+};
+
+MultiSelect.parameters = {
+  docs: {
+    source: {
+      code: `
+      const countries = [
+        { label: 'Indonesia', value: 'Indonesia' },
+        { label: 'Malaysia', value: 'Malaysia' },
+        { label: 'Singapore', value: 'Singapore' },
+        { label: 'Taiwan', value: 'Taiwan' },
+        { label: 'Vietnam', value: 'Vietnam' },
+      ];
+
+      const [inputValue, setInputValue] = useState('');
+      const [selectedOptions, setSelectedOptions] = useState([]);
+      const [isSearchEmpty, setIsSearchEmpty] = useState(false);
+    
+      const [options, setOptions] = useState(countries);
+    
+      const handleInputChange = (value: string) => {
+        setInputValue(value);
+    
+        if (value === '') {
+          setOptions(countries);
+          return;
+        }
+    
+        const filterRegex = new RegExp(value, 'i');
+        const filterOptions = options.filter(country =>
+          country.label.match(filterRegex)
+        );
+        setOptions(filterOptions);
+      };
+    
+      const handleSelect = (selected: string) => {
+        if (selectedOptions.includes(selected)) {
+          setSelectedOptions(selectedOptions.filter(option => option !== selected));
+        } else {
+          setSelectedOptions([...selectedOptions, selected]);
+        }
+      };
+    
+      const optionsMarkup =
+        options.length > 0
+          ? options.map(option => {
+              const { label, value } = option;
+    
+              return (
+                <ComboBox.Option
+                  key={value}
+                  label={label}
+                  value={value}
+                  selected={selectedOptions.includes(value)}
+                />
+              );
+            })
+          : null;
+    
+      const removeTag = useCallback(
+        tag => () => {
+          const options = [...selectedOptions];
+          options.splice(options.indexOf(tag), 1);
+          setSelectedOptions(options);
+        },
+        [selectedOptions]
+      );
+    
+      const tagsMarkup = selectedOptions.map(option => (
+        <StyledTag
+          key={\`option-\${option}\`}
+          onRemove={removeTag(option)}
+          textColor={Blue.S99}
+        >
+          {option}
+        </StyledTag>
+      ));
+    
+      useEffect(() => {
+        if (options.length === 0) {
+          setIsSearchEmpty(true);
+        }
+    
+        if (options.length > 0 && isSearchEmpty === true) {
+          setIsSearchEmpty(false);
+        }
+      }, [isSearchEmpty, options]);
+    
+      return (
+        <div style={{ width: '500px' }}>
+          <ComboBox.Label>Label</ComboBox.Label>
+          <ComboBox
+            {...args}
+            activator={
+              <ComboBox.TextInput
+                value={inputValue}
+                onChange={(value: string) => handleInputChange(value)}
+                placeholder="Search"
+              />
+            }
+          >
+            <ComboBox.OptionList onSelect={handleSelect} isEmpty={isSearchEmpty}>
+              {optionsMarkup}
+            </ComboBox.OptionList>
+          </ComboBox>
+          <div style={{ paddingTop: space8 }}>{tagsMarkup}</div>
+        </div>
+      );
+      `,
+    },
+  },
 };
