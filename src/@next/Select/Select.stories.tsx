20f15491f8d8b20afcccb528fbf56677d3a9e70d
Nina Benite <nina.benite@glints.com>
2023-03-10 14:44:01 +0800

    add Select stories

--- src/@next/Select/Select.stories.tsx
+++ src/@next/Select/Select.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, Story } from '@storybook/react';
 import React from 'react';
 
 import { BaseContainer } from '../../Layout/GlintsContainer/GlintsContainer';
+import { InlineError } from '../InlineError';
 import { Select, SelectProps } from './Select';
 
 (Select as React.FunctionComponent<SelectProps>).displayName = 'Select';
@@ -12,10 +13,32 @@ export default {
   decorators: [Story => <BaseContainer>{Story()}</BaseContainer>],
 } as Meta;
 
+const options = ['Product Designer', 'UI/UX Designer'];
+
 const Template: Story<SelectProps> = args => {
-  return <Select {...args}>Select</Select>;
+  return (
+    <Select {...args} style={{ width: '200px' }} options={options}>
+      Select
+    </Select>
+  );
 };
 
 export const Interactive = Template.bind({});
 
-Interactive.args = {};
+Interactive.args = {
+  placeholder: 'Placeholder',
+  label: 'Label',
+  helpText: 'Help text',
+};
+
+const WithInlineErrorTemplate: Story<SelectProps> = args => {
+  return <Select {...args} style={{ width: '200px' }} options={options} />;
+};
+
+export const WithInlineError = WithInlineErrorTemplate.bind({});
+WithInlineError.args = {
+  placeholder: 'Placeholder',
+  label: 'Label',
+  helpText: <InlineError text="Error message" />,
+  hasError: true,
+};
