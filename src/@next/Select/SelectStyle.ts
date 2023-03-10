85cda94bd8551674d916af1e44b3a63d3c74808f
Nina Benite <nina.benite@glints.com>
2023-03-10 14:43:42 +0800

    add Select styles

--- src/@next/Select/SelectStyle.ts
+++ src/@next/Select/SelectStyle.ts
@@ -1,54 +1,59 @@
 import styled from 'styled-components';
-import { Blue, Neutral } from '../utilities/colors';
-import { space2 } from '../utilities/spacing';
+import { Breakpoints } from '..';
+import { borderRadius4 } from '../utilities/borderRadius';
+import { Neutral, Red } from '../utilities/colors';
+import { NotoSans } from '../utilities/fonts';
+import { space4 } from '../utilities/spacing';
 import { SelectProps } from './Select';
 
-export const StyledLabel = styled.a<SelectProps>`
+export const StyledSelect = styled.select<SelectProps>`
   display: flex;
   align-items: center;
-  color: ${Blue.S99};
+  height: 36px;
+  appearance: none;
+  background: url("data:image/svg+xml,<svg height='20px' width='20px' viewBox='0 0 24 24' fill='%23666666' xmlns='http://www.w3.org/2000/svg'><path d='M12 13.1719L16.95 8.22192L18.364 9.63592L12 15.9999L5.63599 9.63592L7.04999 8.22192L12 13.1719Z'/></svg>")
+    no-repeat right 0.8em center / 1.4em;
+  cursor: pointer;
+  padding: 0px 8px 0px 12px;
+  border: 1px solid ${Neutral.B68};
+  box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.05);
+  border-radius: ${borderRadius4};
 
-  &[data-underline='false'] {
-    text-decoration: none;
+  color: ${Neutral.B18};
+  font-family: ${NotoSans}, sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  letter-spacing: 0px;
+
+  @media (max-width: ${Breakpoints.large}) {
+    font-size: 14px;
   }
 
-  &[data-monochrome='true'] {
-    color: ${Neutral.B18};
-
-    &:hover,
-    &:active {
-      color: ${Neutral.B00};
-
-      svg {
-        fill: ${Neutral.B00};
-      }
-    }
-
-    svg {
-      fill: ${Neutral.B18};
-    }
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 0px 1px #ffffff, 0px 0px 0px 3px #6cc9ec;
   }
 
-  &:hover {
-    color: rgba(0, 86, 140, 0.9);
-
-    svg {
-      fill: rgba(0, 86, 140, 0.9);
-    }
+  &:disabled {
+    color: ${Neutral.B85};
+    background: url("data:image/svg+xml,<svg height='20px' width='20px' viewBox='0 0 24 24' fill='%23D4D5D8' xmlns='http://www.w3.org/2000/svg'><path d='M12 13.1719L16.95 8.22192L18.364 9.63592L12 15.9999L5.63599 9.63592L7.04999 8.22192L12 13.1719Z'/></svg>")
+        no-repeat right 0.8em center / 1.4em,
+      ${Neutral.B95};
+    cursor: default;
   }
 
-  &:active {
-    color: ${Blue.S100};
-
-    svg {
-      fill: ${Blue.S100};
-    }
+  &[data-error='true'] {
+    height: 36px;
+    border: 1px solid ${Red.B93};
+    border-radius: ${borderRadius4};
   }
+`;
 
-  svg {
-    fill: ${Blue.S99};
-    margin-left: ${space2};
-    padding-bottom: ${space2};
-    height: 18px;
-  }
+export const Label = styled.label`
+  margin-bottom: ${space4};
+`;
+
+export const HelpTextContainer = styled.div`
+  margin-top: ${space4};
 `;
