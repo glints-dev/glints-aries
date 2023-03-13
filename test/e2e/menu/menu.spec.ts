cc181b06d03966b14f88f069ef236c06042c012b
Nina Benite <nina.benite@glints.com>
2023-03-13 14:45:06 +0800

    Menu: add basic menu without title test

--- test/e2e/menu/menu.spec.ts
+++ test/e2e/menu/menu.spec.ts
@@ -1,9 +1,18 @@
 import { test, expect } from '@playwright/test';
 import { MenuPage } from './menuPage';
 
+test('Menu - basic without title', async ({ page }) => {
+  const menuPage = new MenuPage(page);
+  await menuPage.goto('args=title:');
+  await expect(menuPage.canvas).toHaveScreenshot(
+    'menu-basic-without-title.png'
+  );
+});
+
 test('Menu - basic with disabled option', async ({ page }) => {
   const menuPage = new MenuPage(page);
   await menuPage.goto();
+  await menuPage.options.first().waitFor();
   await expect(menuPage.canvas).toHaveScreenshot(
     'menu-basic-with-disabled-option.png'
   );
