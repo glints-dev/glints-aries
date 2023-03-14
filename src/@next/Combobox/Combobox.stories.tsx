4a815999215baaa7e9e09deeb43c05e8648a971f
Nina Benite <nina.benite@glints.com>
2023-03-14 12:05:03 +0800

    Combobox: scrollable story

--- src/@next/Combobox/Combobox.stories.tsx
+++ src/@next/Combobox/Combobox.stories.tsx
@@ -1,13 +1,10 @@
 import { Meta, Story } from '@storybook/react';
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
+
 import { withGlintsPortalContainer } from '../../helpers/storybook/Decorators';
-
-import { Blue } from '../utilities/colors';
-import { space8 } from '../utilities/spacing';
-
 import { Option, OptionList, TextInput } from './components';
 import { Combobox, ComboboxProps } from './Combobox';
-import { StyledTag } from './comboboxStoryHelper/TagStyle';
+import { ComboboxMultiSelect } from './comboboxStoryHelper/ComboboxMultuSelect';
 
 (Combobox as React.FunctionComponent<ComboboxProps>).displayName = 'Combobox';
 
@@ -30,102 +27,11 @@ const countries = [
   { label: 'Vietnam', value: 'Vietnam' },
 ];
 
-const MultiSelectTemplate: Story<ComboboxProps> = args => {
-  const [inputValue, setInputValue] = useState('');
-  const [selectedOptions, setSelectedOptions] = useState([]);
-  const [isSearchEmpty, setIsSearchEmpty] = useState(false);
+const slicedCountries = countries.slice(0, 4);
 
-  const [options, setOptions] = useState(countries);
-
-  const handleInputChange = (value: string) => {
-    setInputValue(value);
-
-    if (value === '') {
-      setOptions(countries);
-      return;
-    }
-
-    const filterRegex = new RegExp(value, 'i');
-    const filterOptions = options.filter(country =>
-      country.label.match(filterRegex)
-    );
-    setOptions(filterOptions);
-  };
-
-  const handleSelect = (selected: string) => {
-    if (selectedOptions.includes(selected)) {
-      setSelectedOptions(selectedOptions.filter(option => option !== selected));
-    } else {
-      setSelectedOptions([...selectedOptions, selected]);
-    }
-  };
-
-  const optionsMarkup =
-    options?.length > 0
-      ? options.map(option => {
-          const { label, value } = option;
-
-          return (
-            <Combobox.Option
-              key={value}
-              label={label}
-              value={value}
-              selected={selectedOptions.includes(value)}
-            />
-          );
-        })
-      : null;
-
-  const removeTag = useCallback(
-    tag => () => {
-      const options = [...selectedOptions];
-      options.splice(options.indexOf(tag), 1);
-      setSelectedOptions(options);
-    },
-    [selectedOptions]
-  );
-
-  const tagsMarkup = selectedOptions.map(option => (
-    <StyledTag
-      key={`option-${option}`}
-      onRemove={removeTag(option)}
-      textColor={Blue.S99}
-    >
-      {option}
-    </StyledTag>
-  ));
-
-  useEffect(() => {
-    if (options.length === 0) {
-      setIsSearchEmpty(true);
-    }
-
-    if (options.length > 0 && isSearchEmpty === true) {
-      setIsSearchEmpty(false);
-    }
-  }, [isSearchEmpty, options]);
-
-  return (
-    <div style={{ maxWidth: '500px' }}>
-      <Combobox.Label>Label</Combobox.Label>
-      <Combobox
-        {...args}
-        activator={
-          <Combobox.TextInput
-            value={inputValue}
-            onChange={(value: string) => handleInputChange(value)}
-            placeholder="Search"
-          />
-        }
-      >
-        <Combobox.OptionList onSelect={handleSelect} isEmpty={isSearchEmpty}>
-          {optionsMarkup}
-        </Combobox.OptionList>
-      </Combobox>
-      <div style={{ paddingTop: space8 }}>{tagsMarkup}</div>
-    </div>
-  );
-};
+const MultiSelectTemplate: Story<ComboboxProps> = args => (
+  <ComboboxMultiSelect {...args} countries={slicedCountries} />
+);
 
 export const MultiSelect = MultiSelectTemplate.bind({});
 
@@ -142,7 +48,6 @@ MultiSelect.parameters = {
         { label: 'Malaysia', value: 'Malaysia' },
         { label: 'Singapore', value: 'Singapore' },
         { label: 'Taiwan', value: 'Taiwan' },
-        { label: 'Vietnam', value: 'Vietnam' },
       ];
 
       const [inputValue, setInputValue] = useState('');
@@ -242,4 +147,14 @@ MultiSelect.parameters = {
       `,
     },
   },
+};
+
+const MultiSelectScrollableTemplate: Story<ComboboxProps> = args => (
+  <ComboboxMultiSelect {...args} countries={countries} />
+);
+
+export const MultiSelectScrollable = MultiSelectScrollableTemplate.bind({});
+
+MultiSelectScrollable.args = {
+  allowMultiple: true,
 };
