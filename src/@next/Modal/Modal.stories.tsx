42cf972d2bd09e2abc377a52e615ad88bcf102b2
Nina Benite <nina.benite@glints.com>
2023-03-22 14:28:22 +0800

    Modal: use readable example

--- src/@next/Modal/Modal.stories.tsx
+++ src/@next/Modal/Modal.stories.tsx
@@ -64,19 +64,32 @@ const WithLargeContentTemplate: Story<void> = () => {
     return () => clearTimeout(timeId);
   }, []);
   return (
-    <Modal isOpen={iOpen} header="Title">
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-      doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
-      inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-      Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-      fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
-      sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-      amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
-      incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
-      minima veniam, quis nostrum exercitationem ullam corporis suscipit
-      laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum
-      iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-      consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+    <Modal isOpen={iOpen} header="Desiderata">
+      Go placidly amid the noise and the haste, and remember what peace there
+      may be in silence. As far as possible, without surrender, be on good terms
+      with all persons. Speak your truth quietly and clearly; and listen to
+      others, even to the dull and the ignorant; they too have their story.
+      Avoid loud and aggressive persons; they are vexatious to the spirit. If
+      you compare yourself with others, you may become vain or bitter, for
+      always there will be greater and lesser persons than yourself. Enjoy your
+      achievements as well as your plans. Keep interested in your own career,
+      however humble; it is a real possession in the changing fortunes of time.
+      Exercise caution in your business affairs, for the world is full of
+      trickery. But let this not blind you to what virtue there is; many persons
+      strive for high ideals, and everywhere life is full of heroism. Be
+      yourself. Especially do not feign affection. Neither be cynical about
+      love; for in the face of all aridity and disenchantment, it is as
+      perennial as the grass. Take kindly the counsel of the years, gracefully
+      surrendering the things of youth. Nurture strength of spirit to shield you
+      in sudden misfortune. But do not distress yourself with dark imaginings.
+      Many fears are born of fatigue and loneliness. Beyond a wholesome
+      discipline, be gentle with yourself. You are a child of the universe no
+      less than the trees and the stars; you have a right to be here. And
+      whether or not it is clear to you, no doubt the universe is unfolding as
+      it should. Therefore be at peace with God, whatever you conceive Him to
+      be. And whatever your labors and aspirations, in the noisy confusion of
+      life, keep peace in your soul. With all its sham, drudgery and broken
+      dreams, it is still a beautiful world. Be cheerful. Strive to be happy.
     </Modal>
   );
 };
