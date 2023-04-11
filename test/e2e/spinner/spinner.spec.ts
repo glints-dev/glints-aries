53cfda2a9a7172ac276ab9bd98cfe7e073e497a4
Nina Benite <nina.benite@glints.com>
2023-04-11 18:07:14 +0800

    Spinner: add and update tests

--- test/e2e/spinner/spinner.spec.ts
+++ test/e2e/spinner/spinner.spec.ts
@@ -4,33 +4,72 @@ import { StoryBookPage } from '../storybookPage';
 const getPage = (page: Page) =>
   new StoryBookPage(page, '?path=/story/next-spinner--default');
 
-const getWithLabelPage = (page: Page) =>
-  new StoryBookPage(page, '?path=/story/next-spinner--with-label');
+const getLoadingWithLabelPage = (page: Page) =>
+  new StoryBookPage(page, '?path=/story/next-spinner--loading-with-label');
 
-test('Spinner - large', async ({ page }) => {
+const getProcessingWithLabelPage = (page: Page) =>
+  new StoryBookPage(page, '?path=/story/next-spinner--processing-with-label');
+
+test('Spinner - Loading large', async ({ page }) => {
   const spinnerPage = getPage(page);
   await spinnerPage.goto();
-  await expect(spinnerPage.container).toHaveScreenshot('spinner-large.png');
-});
-
-test('Spinner - large with label', async ({ page }) => {
-  const cardPage = getWithLabelPage(page);
-  await cardPage.goto();
-  await expect(cardPage.container).toHaveScreenshot(
-    'spinner-large-with-label.png'
+  await expect(spinnerPage.container).toHaveScreenshot(
+    'spinner-loading-large.png'
   );
 });
 
-test('Spinner - small', async ({ page }) => {
-  const spinnerPage = getPage(page);
-  await spinnerPage.goto('args=size:small');
-  await expect(spinnerPage.container).toHaveScreenshot('spinner-small.png');
+test('Spinner - Loading large with label', async ({ page }) => {
+  const cardPage = getLoadingWithLabelPage(page);
+  await cardPage.goto();
+  await expect(cardPage.container).toHaveScreenshot(
+    'spinner-loading-large-with-label.png'
+  );
 });
 
-test('Spinner - small with label', async ({ page }) => {
-  const cardPage = getWithLabelPage(page);
+test('Spinner - Loading small', async ({ page }) => {
+  const spinnerPage = getPage(page);
+  await spinnerPage.goto('args=size:small');
+  await expect(spinnerPage.container).toHaveScreenshot(
+    'spinner-loading-small.png'
+  );
+});
+
+test('Spinner - Loading small with label', async ({ page }) => {
+  const cardPage = getLoadingWithLabelPage(page);
   await cardPage.goto('args=size:small');
   await expect(cardPage.container).toHaveScreenshot(
-    'spinner-small-with-label.png'
+    'spinner-loading-small-with-label.png'
+  );
+});
+
+test('Spinner - Processing large', async ({ page }) => {
+  const spinnerPage = getPage(page);
+  await spinnerPage.goto('args=variant:processing');
+  await expect(spinnerPage.container).toHaveScreenshot(
+    'spinner-processing-large.png'
+  );
+});
+
+test('Spinner - Processing large with label', async ({ page }) => {
+  const cardPage = getProcessingWithLabelPage(page);
+  await cardPage.goto();
+  await expect(cardPage.container).toHaveScreenshot(
+    'spinner-processing-large-with-label.png'
+  );
+});
+
+test('Spinner - Processing small', async ({ page }) => {
+  const spinnerPage = getPage(page);
+  await spinnerPage.goto('args=size:small;variant:processing');
+  await expect(spinnerPage.container).toHaveScreenshot(
+    'spinner-processing-small.png'
+  );
+});
+
+test('Spinner - Processing small with label', async ({ page }) => {
+  const cardPage = getProcessingWithLabelPage(page);
+  await cardPage.goto('args=size:small;variant:processing');
+  await expect(cardPage.container).toHaveScreenshot(
+    'spinner-processing-small-with-label.png'
   );
 });
