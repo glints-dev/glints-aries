df864d40183161cc030b059c189e6c26d8387c09
michael <michaelac978@gmail.com>
2023-05-10 14:13:01 +0800

    orange & red color adjustments

--- src/@next/utilities/colors/red.ts
+++ src/@next/utilities/colors/red.ts
@@ -1,6 +1,6 @@
 export const Brand = '#EC272B';
-export const B100 = '#FFC7C2';
-export const B99 = '#FC6568';
+export const B100 = '#FFEEEE';
+export const B99 = '#FCCACB';
 export const B93 = '#EC272B';
 export const B74 = '#BD3638';
 export const B65 = '#8F393B';
