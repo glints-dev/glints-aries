27c0687da56e931d5594f26e470800eb8d8182f1
root <root@LAPTOP-501VT9CE>
2023-05-11 14:56:56 +0800

    adjust red color

--- src/@next/utilities/colors/red.ts
+++ src/@next/utilities/colors/red.ts
@@ -1,5 +1,5 @@
 export const Brand = '#EC272B';
-export const B100 = '#FFEEEE';
+export const B100 = '#FFE5E6';
 export const B99 = '#FCCACB';
 export const B93 = '#EC272B';
 export const B74 = '#BD3638';
