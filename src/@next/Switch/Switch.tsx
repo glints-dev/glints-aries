94c6c225f1e579bff922fd3c6c8bc87a5c31bdfe
Afzal <afzal@glints.com>
2023-05-23 18:28:24 +0530

    feat: adds support to display text in the switch component

--- src/@next/Switch/Switch.tsx
+++ src/@next/Switch/Switch.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Icon } from '../Icon';
 import { Neutral } from '../utilities/colors';
-import { InputStyle, LabelStyle, SwitchStyle } from './SwitchStyle';
+import {
+  InputStyle,
+  LabelStyle,
+  SwitchStyle,
+  SwitchTextStyle,
+} from './SwitchStyle';
 
 export interface SwitchProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
@@ -10,6 +15,9 @@ export interface SwitchProps
   onChange: () => void;
   value: string;
   withIcon?: boolean;
+  checkedText?: string;
+  uncheckedText?: string;
+  checkedBackgroundColor?: string;
 }
 
 export const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
@@ -20,15 +28,43 @@ export const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
       onChange,
       value,
       withIcon,
+      checkedText,
+      uncheckedText,
+      checkedBackgroundColor,
       ...otherProps
     }: SwitchProps,
     ref
   ) {
+    const withText = Boolean(checkedText) && Boolean(uncheckedText);
+
+    if (withText && withIcon) {
+      console.warn('Switch component cannot have both text and icon');
+    }
+
     const IconRender = () => {
       return (
         <>
           <Icon name="ri-check" fill={disabled ? Neutral.B85 : Neutral.B100} />
           <Icon name="ri-close" fill={disabled ? Neutral.B85 : Neutral.B100} />
+        </>
+      );
+    };
+
+    const RenderSwitchText = () => {
+      return (
+        <>
+          <SwitchTextStyle
+            variant="overline"
+            color={disabled ? Neutral.B85 : Neutral.B100}
+          >
+            {checkedText}
+          </SwitchTextStyle>
+          <SwitchTextStyle
+            variant="overline"
+            color={disabled ? Neutral.B85 : Neutral.B100}
+          >
+            {uncheckedText}
+          </SwitchTextStyle>
         </>
       );
     };
@@ -43,10 +79,17 @@ export const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
           type="checkbox"
           onChange={onChange}
           ref={ref}
-          data-with-icon={withIcon}
+          data-with-icon={!withText && withIcon}
+          data-with-text={withText}
+          backgroundColor={checkedBackgroundColor}
         />
-        <SwitchStyle data-disabled={disabled} data-with-icon={withIcon}>
-          {withIcon && <IconRender />}
+        <SwitchStyle
+          data-disabled={disabled}
+          data-with-icon={!withText && withIcon}
+          data-with-text={withText}
+        >
+          {!withText && withIcon && <IconRender />}
+          {withText && <RenderSwitchText />}
         </SwitchStyle>
       </LabelStyle>
     );
