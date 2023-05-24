83df9c0fd6e4cc3f10b5786bd5836018a68a1fe0
Afzal <afzal@glints.com>
2023-05-24 09:01:53 +0530

    test: adds test cases for switch with text

---
+++ test/e2e/switch/switch-with-text.spec.ts
@@ -0,0 +1,67 @@
+import { StoryBookPage } from '../storybookPage';
+import { Page, test, expect } from '@playwright/test';
+
+const getPage = (page: Page) =>
+  new StoryBookPage(page, '?path=/story/next-switch--with-text');
+
+test('default', async ({ page }) => {
+  const switchPage = getPage(page);
+  await switchPage.goto();
+  await expect(switchPage.container).toHaveScreenshot('switch.png');
+});
+
+test('checked', async ({ page }) => {
+  const switchPage = getPage(page);
+  await switchPage.goto('args=checked:true');
+  await expect(switchPage.container).toHaveScreenshot('switch-checked.png');
+});
+
+test('disabled and unchecked', async ({ page }) => {
+  const switchPage = getPage(page);
+  await switchPage.goto('args=disabled:true;checked:false');
+  await expect(switchPage.container).toHaveScreenshot(
+    'switch-disabled-unchecked.png'
+  );
+});
+
+test('disabled and checked', async ({ page }) => {
+  const switchPage = getPage(page);
+  await switchPage.goto('args=checked:true;disabled:true');
+  await expect(switchPage.container).toHaveScreenshot(
+    'switch-disabled-checked.png'
+  );
+});
+
+test('mobile - default', async ({ page }) => {
+  page.setViewportSize({ width: 768, height: 600 });
+  const switchPage = getPage(page);
+  await switchPage.goto();
+  await expect(switchPage.container).toHaveScreenshot('switch-mobile.png');
+});
+
+test('mobile - checked', async ({ page }) => {
+  page.setViewportSize({ width: 768, height: 600 });
+  const switchPage = getPage(page);
+  await switchPage.goto('args=checked:true');
+  await expect(switchPage.container).toHaveScreenshot(
+    'switch-mobile-checked.png'
+  );
+});
+
+test('mobile - disabled and unchecked', async ({ page }) => {
+  page.setViewportSize({ width: 768, height: 600 });
+  const switchPage = getPage(page);
+  await switchPage.goto('args=disabled:true;checked:false');
+  await expect(switchPage.container).toHaveScreenshot(
+    'switch-mobile-disabled-unchecked.png'
+  );
+});
+
+test('mobile - disabled and checked', async ({ page }) => {
+  page.setViewportSize({ width: 768, height: 600 });
+  const switchPage = getPage(page);
+  await switchPage.goto('args=checked:true;disabled:true');
+  await expect(switchPage.container).toHaveScreenshot(
+    'switch-mobile-disabled-checked.png'
+  );
+});
