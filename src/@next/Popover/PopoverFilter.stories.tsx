cbdd632f2b77df8174cfc7ec60d10d6848b26584
Nina Benite <nina.benite@glints.com>
2023-06-05 17:45:23 +0800

    bump polaris-glints

--- src/@next/Popover/PopoverFilter.stories.tsx
+++ src/@next/Popover/PopoverFilter.stories.tsx
@@ -87,6 +87,8 @@ const PopoverAsFilterExample = () => {
           activator={cityActivator}
           autofocusTarget="first-node"
           onClose={togglePopoverCityActive}
+          fullWidth
+          fitContent
         >
           <Popover.Pane fixed>
             <Popover.Section>
