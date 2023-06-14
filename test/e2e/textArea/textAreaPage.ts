91ba4eb62ae661803d490079a0987ceecf32e948
Michael Andrew Chan <michaelac978@gmail.com>
2023-06-14 13:55:34 +0800

    TextArea: testing page delete unused code

--- test/e2e/textArea/textAreaPage.ts
+++ test/e2e/textArea/textAreaPage.ts
@@ -1,19 +1,13 @@
 import { Locator, Page } from '@playwright/test';
-import { Args, StoryBookPage } from '../storybookPage';
+import { StoryBookPage } from '../storybookPage';
 
 export class TextAreaPage extends StoryBookPage {
   readonly text: Locator;
-  readonly clearButton: Locator;
 
   constructor(page: Page) {
     super(page, '?path=/story/next-textarea--interactive');
     this.text = page
       .frameLocator('internal:attr=[title="storybook-preview-iframe"i]')
       .getByRole('textbox');
-  }
-
-  async gotoWithPopulatedValuePage(args?: Args) {
-    this.setPath('?path=/story/next-textarea--with-populated-value');
-    await this.goto(args);
   }
 }
