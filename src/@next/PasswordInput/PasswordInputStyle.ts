72a942ba02301c70b2dacb8490b948ad527c9f7f
Michael Andrew Chan <michaelac978@gmail.com>
2023-06-16 16:03:54 +0800

    PasswordInput: update style based on review

--- src/@next/PasswordInput/PasswordInputStyle.ts
+++ src/@next/PasswordInput/PasswordInputStyle.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
 export const StyledSuffixContainer = styled.div`
-  height: 17px;
+  height: 20px;
+  transform: translateX(4px);
+
   svg {
     cursor: pointer;
+    height: 20px;
+    width: 20px;
   }
 `;
