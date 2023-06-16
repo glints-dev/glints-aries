668b90bb0270a4b53e244ed0d112fe8d43111497
Michael Andrew Chan <michaelac978@gmail.com>
2023-06-16 16:33:50 +0800

    PasswordInput: see last typed character briefly on mobile

--- src/@next/PasswordInput/PasswordInput.tsx
+++ src/@next/PasswordInput/PasswordInput.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Icon } from '../Icon';
 import { Input } from '../Input/Input';
 import { StyledSuffixContainer } from './PasswordInputStyle';
 
 export type PasswordInputProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
-  'type' | 'onChange'
+  'type' | 'onChange' | 'value'
 > & {
   onChange?: (value: string) => void;
   error?: boolean;
+  value?: string;
 };
 
 export const PasswordInput = React.forwardRef<
@@ -19,6 +20,52 @@ export const PasswordInput = React.forwardRef<
   ref
 ) {
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [maskedPassword, setMaskedPassword] = useState<string>(value);
+
+  const pointGen = (pattern: string, num: number) => {
+    return [...Array(num)].map(() => pattern).join('');
+  };
+
+  const DELAY = 1000;
+  const PATTERN = '•';
+  useEffect(() => {
+    const isMobile: boolean = /iPhone|iPad|iPod|Android/i.test(
+      navigator.userAgent
+    );
+    const timer = window.setTimeout(
+      () => {
+        setMaskedPassword(pointGen(PATTERN, value.length));
+      },
+      isMobile ? DELAY : 0
+    );
+    return () => window.clearTimeout(timer);
+  }, [value]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const index = e.target.selectionStart;
+    const inputText = e.target.value;
+    const addedTextLength = inputText.length - value.length;
+
+    if (addedTextLength > 0) {
+      const newStr = inputText.slice(index - addedTextLength, index);
+      onChange?.(
+        value.slice(0, index - addedTextLength) +
+          newStr +
+          value.slice(index - addedTextLength)
+      );
+    } else if (addedTextLength < 0) {
+      onChange?.(value.slice(0, index) + value.slice(index - addedTextLength));
+    }
+
+    if (inputText.length === 0) {
+      setMaskedPassword('');
+    } else {
+      setMaskedPassword(
+        pointGen(PATTERN, inputText.length - 1) +
+          inputText.charAt(inputText.length - 1)
+      );
+    }
+  };
 
   const suffixComponent = (
     <StyledSuffixContainer
@@ -39,9 +86,9 @@ export const PasswordInput = React.forwardRef<
     <>
       <Input
         ref={ref}
-        type={showPassword ? 'text' : 'password'}
-        value={value}
-        onChange={e => onChange?.(e.currentTarget.value)}
+        type="text"
+        value={showPassword ? value : maskedPassword}
+        onChange={handleChange}
         error={error}
         suffix={!props.disabled && suffixComponent}
         {...props}
