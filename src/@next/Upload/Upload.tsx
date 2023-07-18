0684023f901a7b5938bd6b6399cc19100fccfb1e
Gani Ilham Irsyadi <ganiilhamirsyadi@gmail.com>
2023-07-18 11:26:02 +0700

    feat: alter conditional statement

--- src/@next/Upload/Upload.tsx
+++ src/@next/Upload/Upload.tsx
@@ -51,8 +51,8 @@ export const Upload = React.forwardRef<HTMLInputElement, UploadProps>(
         setIsLoading(false);
       };
       if (file) reader.readAsDataURL(file);
-      else setIsLoading(false);
-    }, [file]);
+      else if (!loading) setIsLoading(false);
+    }, [file, loading]);
 
     const handleClick = () => {
       if (fileInputRef.current) {
