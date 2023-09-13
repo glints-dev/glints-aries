7ac6a388f9ef5bbf4612e2db650b80d9cc28153b
Marthayaputra <marthayaputra@glints.com>
2023-09-13 09:32:23 +0800

    fix: use useEffect to trigger change event

--- src/@next/AuthenticationInput/AuthenticationInput.tsx
+++ src/@next/AuthenticationInput/AuthenticationInput.tsx
@@ -24,7 +24,7 @@ const AuthenticationInputComponent = ({
 
   useEffect(() => {
     if (inputValues.some(v => !Number.isInteger(v))) {
-      onChange?.(null);
+      onChange?.('');
       return;
     }
     onChange?.(inputValues.join(''));
