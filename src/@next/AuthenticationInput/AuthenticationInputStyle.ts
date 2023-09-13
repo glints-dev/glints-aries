3396ccefb1b54eeeda7d3a80d6fdbdda81c06809
Marthayaputra <marthayaputra@glints.com>
2023-09-13 11:23:51 +0800

    fix: gap

--- src/@next/AuthenticationInput/AuthenticationInputStyle.ts
+++ src/@next/AuthenticationInput/AuthenticationInputStyle.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import { space4 } from '../utilities/spacing';
+import { space8 } from '../utilities/spacing';
 
 export const StyledInputsContainer = styled.div`
   display: flex;
-  gap: ${space4};
+  gap: ${space8};
   width: fit-content;
 
   .authentication-input {
