55e59771b7e7cf0598cce6c29bf2da97756ff594
Muhammad Farhan Abiyyullah <farhanabiyullah@gmail.com>
2023-12-13 13:42:44 +0700

    feat: Allow non-string children to options

---
+++ src/Input/Select/Select.numberChecking.test.tsx
@@ -0,0 +1,118 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+import * as React from 'react';
+import 'jest-styled-components';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Select from './Select';
+
+describe('<Select /> can still work when receives number option children or value', () => {
+  test('number option children, click option', async () => {
+    const onChange = jest.fn();
+
+    const Component = () => (
+      <Select label="number option children" onChange={onChange}>
+        <Select.Option value="one">{1}</Select.Option>
+        <Select.Option value="two">{2}</Select.Option>
+        <Select.Option value="three">{3}</Select.Option>
+      </Select>
+    );
+
+    const { queryByText, asFragment } = render(<Component />);
+
+    const downArrowIcon = document.querySelector('.select-icon');
+    fireEvent.click(downArrowIcon);
+    const option1 = queryByText('1');
+    fireEvent.mouseDown(option1);
+
+    expect(onChange).toHaveBeenLastCalledWith('one');
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('number option children, search filtering', async () => {
+    const Component = () => (
+      <Select label="number option children">
+        <Select.Option value="one">{1}</Select.Option>
+        <Select.Option value="two">{2}</Select.Option>
+        <Select.Option value="three">{3}</Select.Option>
+      </Select>
+    );
+
+    const { queryByRole, queryAllByTestId, asFragment } = render(<Component />);
+
+    const selectInput = queryByRole('combobox');
+    userEvent.type(selectInput, '1');
+    const filteredOptions = queryAllByTestId('option');
+    expect(filteredOptions.length).toEqual(1);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('number defaultValue', async () => {
+    const testNumberDefaultValue = ([defaultValue, expectedValue]: any) => {
+      const Component = () => (
+        <Select label="number defaultValue" defaultValue={defaultValue}>
+          <Select.Option value="one">{1}</Select.Option>
+          <Select.Option value="two">{2}</Select.Option>
+          <Select.Option value="three">{3}</Select.Option>
+        </Select>
+      );
+
+      const { queryByRole, unmount } = render(<Component />);
+      const selectInput = queryByRole('combobox') as HTMLInputElement;
+      expect(selectInput.value).toEqual(expectedValue);
+      unmount();
+    };
+
+    [
+      [1, '1'],
+      [[1, 2, 3], '1,2,3'],
+    ].forEach(([defaultValue, expectedValue]) => {
+      testNumberDefaultValue([defaultValue, expectedValue]);
+    });
+  });
+
+  test('number value', async () => {
+    const testNumberValue = ([value, expectedValue]: any) => {
+      const Component = () => (
+        <Select label="number defaultValue" value={value}>
+          <Select.Option value="one">{1}</Select.Option>
+          <Select.Option value="two">{2}</Select.Option>
+          <Select.Option value="three">{3}</Select.Option>
+        </Select>
+      );
+
+      const { queryByRole, unmount } = render(<Component />);
+      const selectInput = queryByRole('combobox') as HTMLInputElement;
+      expect(selectInput.value).toEqual(expectedValue);
+      unmount();
+    };
+
+    [
+      [1, '1'],
+      [[1, 2, 3], '1,2,3'],
+    ].forEach(([value, expectedValue]) => {
+      testNumberValue([value, expectedValue]);
+    });
+  });
+
+  test('rerender with number value', async () => {
+    const Component = ({ value }: { value: string }) => (
+      <Select label="rerender" value={value}>
+        <Select.Option value="rerender">Rerender</Select.Option>
+      </Select>
+    );
+
+    const { queryByRole, rerender } = render(<Component value="Rerender" />);
+    const selectInput = queryByRole('combobox');
+    expect(selectInput.getAttribute('value')).toEqual('Rerender');
+
+    rerender(<Component value={1} />);
+    expect(selectInput.getAttribute('value')).toEqual('1');
+
+    rerender(<Component value={[1, 2, 3]} />);
+    expect(selectInput.getAttribute('value')).toEqual('1,2,3');
+  });
+});
